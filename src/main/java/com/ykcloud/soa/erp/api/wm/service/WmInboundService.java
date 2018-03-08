107f00c544aa9f9a6978afa3def84741614b7dc6
shirong.chen <shirong.chen@yiako.com>
2018-03-08 23:20:44 +0800

    删除范例代码

--- src/main/java/com/ykcloud/soa/erp/api/wm/service/WmInboundService.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/service/WmInboundService.java
@@ -1,12 +1,7 @@
 package com.ykcloud.soa.erp.api.wm.service;
 
-import com.ykcloud.soa.erp.api.wm.request.GoodsByBarcodeGetRequest;
-import com.ykcloud.soa.erp.api.wm.response.GoodsByBarcodeGetResponse;
-
 //入库服务
 public interface WmInboundService {
 
-	//通过条码获取商品信息
-	public GoodsByBarcodeGetResponse getGoodsInfoByBarcode(GoodsByBarcodeGetRequest request);
-
+	
 }
