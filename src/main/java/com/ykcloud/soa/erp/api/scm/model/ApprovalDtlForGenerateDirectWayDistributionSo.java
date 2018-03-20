2c00b5c4b85e84909fcde34f236a5dca4dd4e1f7
shirong.chen <shirong.chen@yiako.com>
2018-03-20 15:33:53 +0800

    增加订货审批单箱规字段

--- src/main/java/com/ykcloud/soa/erp/api/scm/model/ApprovalDtlForGenerateDirectWayDistributionSo.java
+++ src/main/java/com/ykcloud/soa/erp/api/scm/model/ApprovalDtlForGenerateDirectWayDistributionSo.java
@@ -8,9 +8,89 @@ public class ApprovalDtlForGenerateDirectWayDistributionSo implements Serializab
 	
     private Long ordSubUnitNumId; //订货门店
     
+    private Long itemNumId;
+    
+    private Long divNumId;
+    
+    private String itemName;
+    
+    private String barcode;
+    
+    private Long productOriginNumId;
+    
+    private Double conversionQty;
+    
+    private Long conversionUnitNumId;
+    
+    private String conversionStyleDesc;
+        
     private Double qty;
     
     private Double pmtQty;
+    
+	public Long getItemNumId() {
+		return itemNumId;
+	}
+
+	public void setItemNumId(Long itemNumId) {
+		this.itemNumId = itemNumId;
+	}
+
+	public Long getDivNumId() {
+		return divNumId;
+	}
+
+	public void setDivNumId(Long divNumId) {
+		this.divNumId = divNumId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getBarcode() {
+		return barcode;
+	}
+
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+
+	public Long getProductOriginNumId() {
+		return productOriginNumId;
+	}
+
+	public void setProductOriginNumId(Long productOriginNumId) {
+		this.productOriginNumId = productOriginNumId;
+	}
+
+	public Double getConversionQty() {
+		return conversionQty;
+	}
+
+	public void setConversionQty(Double conversionQty) {
+		this.conversionQty = conversionQty;
+	}
+
+	public Long getConversionUnitNumId() {
+		return conversionUnitNumId;
+	}
+
+	public void setConversionUnitNumId(Long conversionUnitNumId) {
+		this.conversionUnitNumId = conversionUnitNumId;
+	}
+
+	public String getConversionStyleDesc() {
+		return conversionStyleDesc;
+	}
+
+	public void setConversionStyleDesc(String conversionStyleDesc) {
+		this.conversionStyleDesc = conversionStyleDesc;
+	}
 
 	public Long getOrdSubUnitNumId() {
 		return ordSubUnitNumId;
