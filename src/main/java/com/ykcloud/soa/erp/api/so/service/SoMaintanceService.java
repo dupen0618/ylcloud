3d364b528c0ee32859f637c27b15db72b2f70636
shirong.chen <shirong.chen@yiako.com>
2018-03-21 12:19:56 +0800

    直通收货按验收单及订货审批单产生分拨单待实现

--- src/main/java/com/ykcloud/soa/erp/api/so/service/SoMaintanceService.java
+++ src/main/java/com/ykcloud/soa/erp/api/so/service/SoMaintanceService.java
@@ -10,7 +10,8 @@ public interface SoMaintanceService {
 	// 按条码获取商品
 	public GoodsByBarcodeGetResponse getGoodsInfoByBarcode(GoodsByBarcodeGetRequest request);
 	
+	//待实现
 	//直通收货按验收单及订货审批单产生分拨单
-	public DirectWayDistributionSoGenerateResponse  generateDirectWayDistributionSo(DirectWayDistributionSoGenerateRequest request);
+	//public DirectWayDistributionSoGenerateResponse  generateDirectWayDistributionSo(DirectWayDistributionSoGenerateRequest request);
 
 }
