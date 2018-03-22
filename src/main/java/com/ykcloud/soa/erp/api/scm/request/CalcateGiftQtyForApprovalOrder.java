af5d9ca0bdfdee46eaf308306eecd2d77ec97b6f
zhaokang <fred.zhao@yiako.com>
2018-03-22 20:54:25 +0800

    分配赠品

---
+++ src/main/java/com/ykcloud/soa/erp/api/scm/request/CalcateGiftQtyForApprovalOrder.java
@@ -0,0 +1,8 @@
+package com.ykcloud.soa.erp.api.scm.request;
+
+public class CalcateGiftQtyForApprovalOrder extends AbstractMethodError {
+
+    private static final long serialVersionUID = -1L;
+
+
+}
