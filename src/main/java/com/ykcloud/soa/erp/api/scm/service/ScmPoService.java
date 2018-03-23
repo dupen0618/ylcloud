7a6adabc0ff4c051792c217036a4b0507818d0ec
shirong.chen <shirong.chen@yiako.com>
2018-03-23 13:47:57 +0800

    为产生直送分拨单获取订货审批单明细

--- src/main/java/com/ykcloud/soa/erp/api/scm/service/ScmPoService.java
+++ src/main/java/com/ykcloud/soa/erp/api/scm/service/ScmPoService.java
@@ -2,21 +2,15 @@
 package com.ykcloud.soa.erp.api.scm.service;
 
 import com.ykcloud.soa.erp.api.scm.request.ApprovalDtlsForGenerateDirectWayDistributionSoGetRequest;
-import com.ykcloud.soa.erp.api.scm.request.PoGenerateRequest;
 import com.ykcloud.soa.erp.api.scm.request.PoForGenerateDirectWayDistributionSoGetRequest;
+import com.ykcloud.soa.erp.api.scm.request.PoGenerateRequest;
 import com.ykcloud.soa.erp.api.scm.response.ApprovalDtlsForGenerateDirectWayDistributionSoGetResponse;
-import com.ykcloud.soa.erp.api.scm.response.PoGenerateResponse;
 import com.ykcloud.soa.erp.api.scm.response.PoForGenerateDirectWayDistributionSoGetResponse;
+import com.ykcloud.soa.erp.api.scm.response.PoGenerateResponse;
 
 public interface ScmPoService {
 	
-	/**
-	 * 通过订货审批单获取采购单号
-	 * @author tz.x
-	 * @date 2018年3月21日下午3:32:00
-	 * @param 
-	 * @return
-	 */
+	//订货审批单产生采购单
 	public PoGenerateResponse generatePo(PoGenerateRequest request);
 	
 	//为产生直送分拨单获取采购单信息
