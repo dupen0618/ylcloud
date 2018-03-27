91481ad95129afdd994710b316beb337318beb8b
henry.wang <henry.wang@yiako.com>
2018-03-27 20:58:25 +0800

    增加盘点服务接口

---
+++ src/main/java/com/ykcloud/soa/erp/api/wm/response/StockCheckPlanAuditResponse.java
@@ -0,0 +1,14 @@
+package com.ykcloud.soa.erp.api.wm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * @ClassName StockCheckPlanAuditResponse
+ * @Description 盘点计划审核的出参
+ * @author henry.wang
+ * @date 2018年3月27日 下午2:16:36
+ */
+public class StockCheckPlanAuditResponse extends MessagePack {
+
+    private static final long serialVersionUID = -8716207623324225827L;
+}
