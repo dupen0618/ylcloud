76868e650aac603ca494cde3a046595df3b32821
zhaokang <fred.zhao@yiako.com>
2018-03-28 13:54:49 +0800

    重新分配赠品接口

---
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/ApprovalReturnOrderGenerateResponse.java
@@ -0,0 +1,10 @@
+package com.ykcloud.soa.erp.api.scm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+public class ApprovalReturnOrderGenerateResponse extends MessagePack{
+
+    private static final long serialVersionUID = -6718227698912185972L;
+
+
+}
