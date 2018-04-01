892a359a89ee21f3b914c5e12928c7febb3076fa
shirong.chen <shirong.chen@yiako.com>
2018-04-01 11:37:06 +0800

    增加配送分拨

---
+++ src/main/java/com/ykcloud/soa/erp/api/so/response/DistributionSoGenerateResponse.java
@@ -0,0 +1,10 @@
+package com.ykcloud.soa.erp.api.so.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+public class DistributionSoGenerateResponse extends MessagePack {
+
+	private static final long serialVersionUID = 1L;
+
+	
+}
--- src/main/java/com/ykcloud/soa/erp/api/so/service/SoMaintanceService.java
+++ src/main/java/com/ykcloud/soa/erp/api/so/service/SoMaintanceService.java
@@ -1,10 +1,12 @@
 package com.ykcloud.soa.erp.api.so.service;
 
 import com.ykcloud.soa.erp.api.so.request.DirectWayDistributionSoGenerateRequest;
+import com.ykcloud.soa.erp.api.so.request.DistributionSoGenerateRequest;
 import com.ykcloud.soa.erp.api.so.request.GoodsByBarcodeGetRequest;
 import com.ykcloud.soa.erp.api.so.request.SoPackingUpdateRequest;
 import com.ykcloud.soa.erp.api.so.request.SoWlbcAndSoBingingRequest;
 import com.ykcloud.soa.erp.api.so.response.DirectWayDistributionSoGenerateResponse;
+import com.ykcloud.soa.erp.api.so.response.DistributionSoGenerateResponse;
 import com.ykcloud.soa.erp.api.so.response.GoodsByBarcodeGetResponse;
 import com.ykcloud.soa.erp.api.so.response.SoPackingUpdateResponse;
 import com.ykcloud.soa.erp.api.so.response.SoWlbcAndSoBingingResponse;
@@ -16,6 +18,9 @@ public interface SoMaintanceService {
 	
 	//直通收货按验收单及订货审批单产生分拨单
 	public DirectWayDistributionSoGenerateResponse  generateDirectWayDistributionSo(DirectWayDistributionSoGenerateRequest request);
+
+	//产生配送分拨单
+	public DistributionSoGenerateResponse generateDistributionSo(DistributionSoGenerateRequest request);
 
 	//更新订单中的装箱数量
 	public SoPackingUpdateResponse updateSoPackingDtl(SoPackingUpdateRequest request);
