a13c1bdc713864c5db9c7844f28667a9d69f4c03
zhaokang <fred.zhao@yiako.com>
2018-04-02 17:50:55 +0800

    修改发送消息接口

---
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/DistributionApprovalReturnOrderByBuyerAuditResponse.java
@@ -0,0 +1,14 @@
+package com.ykcloud.soa.erp.api.scm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * @Author: fred.zhao
+ * @Description:退货审批单退大仓
+ * @Date: Created in 16:48:2018/4/2
+ */
+public class DistributionApprovalReturnOrderByBuyerAuditResponse extends MessagePack {
+    private static final long serialVersionUID = -1664834846903901088L;
+
+
+}
