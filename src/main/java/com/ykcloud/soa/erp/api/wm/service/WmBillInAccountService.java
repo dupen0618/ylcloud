7524027f8f4e882bee2d9dab8a27ad68ceb2e9af
damon.he@yiako.com <damon.he@yiako.com>
2018-04-02 11:08:34 +0800

    更新代码

--- src/main/java/com/ykcloud/soa/erp/api/wm/service/WmBillInAccountService.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/service/WmBillInAccountService.java
@@ -8,10 +8,10 @@ import com.ykcloud.soa.erp.api.wm.response.WmBillInAccountResponse;
  * @Date 2018/3/26 18:51
  */
 public interface WmBillInAccountService {
-    //库存流水
+    //可用库存
     WmBillInAccountResponse ReceiptInAccountTSCService(WmBillInAccountRequest wmBillInAccountRequest);
 
-    //物理仓流水
+    //物理仓
     WmBillInAccountResponse ReceiptInAccountPhysicalWBAService(WmBillInAccountRequest wmBillInAccountRequest);
 
 }
