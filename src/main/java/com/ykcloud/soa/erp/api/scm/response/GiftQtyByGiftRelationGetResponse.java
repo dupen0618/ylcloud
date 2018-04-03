69a6cbb591e129e6c7787b6abde017203a72d13c
shirong.chen <shirong.chen@yiako.com>
2018-04-03 11:53:17 +0800

    赠品计算

--- src/main/java/com/ykcloud/soa/erp/api/scm/response/GiftQtyByGiftRelationGetResponse.java
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/GiftQtyByGiftRelationGetResponse.java
@@ -8,6 +8,12 @@ public class GiftQtyByGiftRelationGetResponse extends MessagePack {
 	private static final long serialVersionUID = -3621634900229807547L;
 	//赠品数量
 	private Double giftQty;
+	
+	private Double qty;
+	
+	private Double packageQty;
+	
+	
 	public Double getGiftQty() {
 		return giftQty;
 	}
@@ -15,6 +21,23 @@ public class GiftQtyByGiftRelationGetResponse extends MessagePack {
 	public void setGiftQty(Double giftQty) {
 		this.giftQty = giftQty;
 	}
+
+	public Double getQty() {
+		return qty;
+	}
+
+	public void setQty(Double qty) {
+		this.qty = qty;
+	}
+
+	public Double getPackageQty() {
+		return packageQty;
+	}
+
+	public void setPackageQty(Double packageQty) {
+		this.packageQty = packageQty;
+	}
+	
 	
 
 }
