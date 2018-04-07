262338d029fd8d3457ebffb50067c763a46979f0
zhaokang <fred.zhao@yiako.com>
2018-04-07 08:55:05 +0800

    4.7更新代码

---
+++ src/main/java/com/ykcloud/soa/erp/api/so/service/SoAppScmReplenishService.java
@@ -0,0 +1,10 @@
+package com.ykcloud.soa.erp.api.so.service;
+
+/**
+ * @Author: fred.zhao
+ * @Description: 退货申请主管审核服务
+ * @Date: Created in 17:44:2018/4/4
+ */
+public class SoAppScmReplenishService {
+
+}
