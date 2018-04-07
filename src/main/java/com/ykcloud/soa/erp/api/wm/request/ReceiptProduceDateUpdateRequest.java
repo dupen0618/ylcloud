0df993ef5fb526ff66eb2db904b779a95553ca03
hank.zhu <hank.zhu@yiako.com>
2018-04-07 09:25:37 +0800

    解决冲突

--- src/main/java/com/ykcloud/soa/erp/api/wm/request/ReceiptProduceDateUpdateRequest.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/request/ReceiptProduceDateUpdateRequest.java
@@ -2,48 +2,50 @@ package com.ykcloud.soa.erp.api.wm.request;
 
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 import com.gb.soa.omp.ccommon.api.annotation.ApiField;
 import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
 
 /**
-*
+*更新生产日期,证照验证响应类
 @author hank.zhu
 @date 2018年3月13日 下午4:19:33 新建
 **/
 //过期控制  证照控制 入参类
 public class ReceiptProduceDateUpdateRequest extends AbstractRequest {
 
-	private static final long serialVersionUID = 8403635836329501861L;
-	
+	private static final long serialVersionUID = 8792481116124137792L;
+
 	@ApiField(description = "验收单号")
 	@NotNull(message = "验收单号不能为空!")
-	private Long reservedNo;
+	private String reservedNo;
 	
 	@ApiField(description = "托盘号")
 	@NotNull(message = "托盘号不能为空!")
-	private Long traySerlno;
+	private String traySerlno;
 	
 	@ApiField(description = "商品ID")
 	@NotNull(message = "商品ID不能为空!")
 	private Long itemNumId;
 	
 	@ApiField(description = "实际生产日期")
-	@NotNull(message = "时期生产日期不能为空!")
-	private Long actualProductionDate;
+	@NotEmpty(message = "时期生产日期不能为空!")
+	private String actualProductionDate;
 
-	public Long getReservedNo() {
+	public String getReservedNo() {
 		return reservedNo;
 	}
 
-	public void setReservedNo(Long reservedNo) {
+	public void setReservedNo(String reservedNo) {
 		this.reservedNo = reservedNo;
 	}
 
-	public Long getTraySerlno() {
+	public String getTraySerlno() {
 		return traySerlno;
 	}
 
-	public void setTraySerlno(Long traySerlno) {
+	public void setTraySerlno(String traySerlno) {
 		this.traySerlno = traySerlno;
 	}
 
@@ -55,20 +57,18 @@ public class ReceiptProduceDateUpdateRequest extends AbstractRequest {
 		this.itemNumId = itemNumId;
 	}
 
-	public Long getActualProductionDate() {
+	public String getActualProductionDate() {
 		return actualProductionDate;
 	}
 
-	public void setActualProductionDate(Long actualProductionDate) {
+	public void setActualProductionDate(String actualProductionDate) {
 		this.actualProductionDate = actualProductionDate;
 	}
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+
 	
-	
-	
-	
-	
+
 }
--- src/main/java/com/ykcloud/soa/erp/api/wm/service/WmReceiptTrayService.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/service/WmReceiptTrayService.java
@@ -1,14 +1,20 @@
 package com.ykcloud.soa.erp.api.wm.service;
 
+import com.ykcloud.soa.erp.api.wm.request.ReceiptTrayGetRequest;
 import com.ykcloud.soa.erp.api.wm.request.ReceiptTrayHdrCreateRequest;
 import com.ykcloud.soa.erp.api.wm.request.ReceiptTrayHdrFinishRequest;
+import com.ykcloud.soa.erp.api.wm.response.ReceiptTrayGetResponse;
 import com.ykcloud.soa.erp.api.wm.response.ReceiptTrayHdrCreateResponse;
 import com.ykcloud.soa.erp.api.wm.response.ReceiptTrayHdrFinishResponse;
 
 public interface WmReceiptTrayService {
-	
-	//创建托盘
+
+	// 创建托盘
 	public ReceiptTrayHdrCreateResponse createReceiptTrayHdr(ReceiptTrayHdrCreateRequest request);
-	//托盘完成(封箱)
+
+	// 托盘完成(封箱)
 	public ReceiptTrayHdrFinishResponse finishReceiptTrayHdr(ReceiptTrayHdrFinishRequest request);
+
+	// 查询托盘
+	public ReceiptTrayGetResponse getReceiptTray(ReceiptTrayGetRequest request);
 }
