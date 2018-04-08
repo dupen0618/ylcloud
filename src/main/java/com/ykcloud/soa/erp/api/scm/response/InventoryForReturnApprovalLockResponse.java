8e3292785b10868812457f411c2860246b350350
xutengzhou <peter.xu@yiako.com>
2018-04-08 16:24:24 +0800

    提交退货审批后锁库及产生so接口

---
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/InventoryForReturnApprovalLockResponse.java
@@ -0,0 +1,15 @@
+package com.ykcloud.soa.erp.api.scm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * 退货审批后锁库及产生so出参
+ * @author tz.x
+ *
+ * @date 2018年4月8日下午4:19:08
+ */
+public class InventoryForReturnApprovalLockResponse extends MessagePack {
+
+	private static final long serialVersionUID = 8727014691726789740L;
+
+}
--- src/main/java/com/ykcloud/soa/erp/api/scm/service/ScmReturnService.java
+++ src/main/java/com/ykcloud/soa/erp/api/scm/service/ScmReturnService.java
@@ -6,12 +6,14 @@ import com.ykcloud.soa.erp.api.scm.request.ApprovalReturnOrderByFinanceAutoAudit
 import com.ykcloud.soa.erp.api.scm.request.ApprovalReturnOrderByFinanceHandAuditRequest;
 import com.ykcloud.soa.erp.api.scm.request.ApprovalReturnOrderGenerateRequest;
 import com.ykcloud.soa.erp.api.scm.request.DistributionApprovalReturnOrderByBuyerAuditRequest;
+import com.ykcloud.soa.erp.api.scm.request.InventoryForReturnApprovalLockRequest;
 import com.ykcloud.soa.erp.api.scm.request.SupplyApprovalReturnOrderByBuyerAuditRequest;
 import com.ykcloud.soa.erp.api.scm.response.ApprovalReturnOrderByFinanceAutoAuditResponse;
 import com.ykcloud.soa.erp.api.scm.response.ApprovalReturnOrderByFinanceHandAuditResponse;
 import com.ykcloud.soa.erp.api.scm.response.ApprovalReturnOrderGenerateResponse;
 import com.ykcloud.soa.erp.api.scm.request.ReturnProductGetRequest;
 import com.ykcloud.soa.erp.api.scm.response.DistributionApprovalReturnOrderByBuyerAuditResponse;
+import com.ykcloud.soa.erp.api.scm.response.InventoryForReturnApprovalLockResponse;
 import com.ykcloud.soa.erp.api.scm.response.ReturnProductGetResponse;
 import com.ykcloud.soa.erp.api.scm.response.SupplyApprovalReturnOrderByBuyerAuditResponse;
 
@@ -20,6 +22,16 @@ import com.ykcloud.soa.erp.api.scm.response.SupplyApprovalReturnOrderByBuyerAudi
  * 根据退货申请产生退货审批单服务
  */
 public interface ScmReturnService {
+	
+	/**
+	 * 退货审批后锁库及产生so
+	 * @author tz.x
+	 * @date 2018年4月8日下午4:20:40
+	 * @param 
+	 * @return
+	 */
+	public InventoryForReturnApprovalLockResponse lockInventoryForReturnApproval(InventoryForReturnApprovalLockRequest request);
+	
     //退货申请单生成退货审批单
     public ApprovalReturnOrderGenerateResponse generateApprovalReturnOrder(ApprovalReturnOrderGenerateRequest request);
      //前台根据商品编号获取退货商品相关信息(价格）
