1d2e4417a3609b334118761ef1c07e2f3752e922
shirong.chen <shirong.chen@yiako.com>
2018-04-08 12:42:38 +0800

    配送商品

--- src/main/java/com/ykcloud/soa/erp/api/scm/model/DistributionProduct.java
+++ src/main/java/com/ykcloud/soa/erp/api/scm/model/DistributionProduct.java
@@ -1,7 +1,6 @@
 package com.ykcloud.soa.erp.api.scm.model;
 
 import java.io.Serializable;
-import java.util.Date;
 
 public class DistributionProduct implements Serializable {
 
@@ -43,7 +42,7 @@ public class DistributionProduct implements Serializable {
 
 	private String storeTypeName;// 储存方式名称
 
-	private Long storageNumId;// 直送、直同收货仓库，配送无意义为0
+	private Long storageNumId;// 配送仓库
 
 	private Double conversionQty;// 件装数
 
@@ -55,14 +54,236 @@ public class DistributionProduct implements Serializable {
 
 	private Double salePrice;// 销售价格
 
-	private Double minQty;// 最小配送数
-
 	private Double stockQty;// 库存数
 
-	private Double balanceCost;// 结存成本
+	private Double cost;// 配送价格
 
-	private Double availbleSaleDays;// 可销天数
+	private Double openDistQty;// 在配数量
 
+	private Double minQty;// 最小配送数
 
-	
+	public Long getItemNumId() {
+		return itemNumId;
+	}
+
+	public void setItemNumId(Long itemNumId) {
+		this.itemNumId = itemNumId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getBarcode() {
+		return barcode;
+	}
+
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+
+	public Long getDivNumId() {
+		return divNumId;
+	}
+
+	public void setDivNumId(Long divNumId) {
+		this.divNumId = divNumId;
+	}
+
+	public String getDivName() {
+		return divName;
+	}
+
+	public void setDivName(String divName) {
+		this.divName = divName;
+	}
+
+	public Long getBasicUnitNumId() {
+		return basicUnitNumId;
+	}
+
+	public void setBasicUnitNumId(Long basicUnitNumId) {
+		this.basicUnitNumId = basicUnitNumId;
+	}
+
+	public String getBasicUnitName() {
+		return basicUnitName;
+	}
+
+	public void setBasicUnitName(String basicUnitName) {
+		this.basicUnitName = basicUnitName;
+	}
+
+	public String getStyleDesc() {
+		return styleDesc;
+	}
+
+	public void setStyleDesc(String styleDesc) {
+		this.styleDesc = styleDesc;
+	}
+
+	public Long getQualityDays() {
+		return qualityDays;
+	}
+
+	public void setQualityDays(Long qualityDays) {
+		this.qualityDays = qualityDays;
+	}
+
+	public Long getGpItemNumId() {
+		return gpItemNumId;
+	}
+
+	public void setGpItemNumId(Long gpItemNumId) {
+		this.gpItemNumId = gpItemNumId;
+	}
+
+	public Long getSettlementType() {
+		return settlementType;
+	}
+
+	public void setSettlementType(Long settlementType) {
+		this.settlementType = settlementType;
+	}
+
+	public String getSettlementTypeName() {
+		return settlementTypeName;
+	}
+
+	public void setSettlementTypeName(String settlementTypeName) {
+		this.settlementTypeName = settlementTypeName;
+	}
+
+	public Long getLogisticsType() {
+		return logisticsType;
+	}
+
+	public void setLogisticsType(Long logisticsType) {
+		this.logisticsType = logisticsType;
+	}
+
+	public String getLogisticsTypeName() {
+		return logisticsTypeName;
+	}
+
+	public void setLogisticsTypeName(String logisticsTypeName) {
+		this.logisticsTypeName = logisticsTypeName;
+	}
+
+	public Long getProductOriginNumId() {
+		return productOriginNumId;
+	}
+
+	public void setProductOriginNumId(Long productOriginNumId) {
+		this.productOriginNumId = productOriginNumId;
+	}
+
+	public String getProductOriginName() {
+		return productOriginName;
+	}
+
+	public void setProductOriginName(String productOriginName) {
+		this.productOriginName = productOriginName;
+	}
+
+	public Long getStoreType() {
+		return storeType;
+	}
+
+	public void setStoreType(Long storeType) {
+		this.storeType = storeType;
+	}
+
+	public String getStoreTypeName() {
+		return storeTypeName;
+	}
+
+	public void setStoreTypeName(String storeTypeName) {
+		this.storeTypeName = storeTypeName;
+	}
+
+	public Long getStorageNumId() {
+		return storageNumId;
+	}
+
+	public void setStorageNumId(Long storageNumId) {
+		this.storageNumId = storageNumId;
+	}
+
+	public Double getConversionQty() {
+		return conversionQty;
+	}
+
+	public void setConversionQty(Double conversionQty) {
+		this.conversionQty = conversionQty;
+	}
+
+	public Long getConversionUnitNumId() {
+		return conversionUnitNumId;
+	}
+
+	public void setConversionUnitNumId(Long conversionUnitNumId) {
+		this.conversionUnitNumId = conversionUnitNumId;
+	}
+
+	public String getConversionUnitName() {
+		return conversionUnitName;
+	}
+
+	public void setConversionUnitName(String conversionUnitName) {
+		this.conversionUnitName = conversionUnitName;
+	}
+
+	public String getConversionStyleDesc() {
+		return conversionStyleDesc;
+	}
+
+	public void setConversionStyleDesc(String conversionStyleDesc) {
+		this.conversionStyleDesc = conversionStyleDesc;
+	}
+
+	public Double getSalePrice() {
+		return salePrice;
+	}
+
+	public void setSalePrice(Double salePrice) {
+		this.salePrice = salePrice;
+	}
+
+	public Double getStockQty() {
+		return stockQty;
+	}
+
+	public void setStockQty(Double stockQty) {
+		this.stockQty = stockQty;
+	}
+
+	public Double getCost() {
+		return cost;
+	}
+
+	public void setCost(Double cost) {
+		this.cost = cost;
+	}
+
+	public Double getOpenDistQty() {
+		return openDistQty;
+	}
+
+	public void setOpenDistQty(Double openDistQty) {
+		this.openDistQty = openDistQty;
+	}
+
+	public Double getMinQty() {
+		return minQty;
+	}
+
+	public void setMinQty(Double minQty) {
+		this.minQty = minQty;
+	}
+
 }
--- src/main/java/com/ykcloud/soa/erp/api/scm/response/DistributionProductInfoGetResponse.java
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/DistributionProductInfoGetResponse.java
@@ -1,21 +1,21 @@
 package com.ykcloud.soa.erp.api.scm.response;
 
 import com.gb.soa.omp.ccommon.api.response.MessagePack;
-import com.ykcloud.soa.erp.api.scm.model.ReplenishProduct;
+import com.ykcloud.soa.erp.api.scm.model.DistributionProduct;
 import java.util.List;
 
 public class DistributionProductInfoGetResponse extends MessagePack {
 
 	private static final long serialVersionUID = 1L;
 
-	private List<ReplenishProduct> data;
+	private List<DistributionProduct> data;
 
-	public List<ReplenishProduct> getData() {
+	public List<DistributionProduct> getData() {
 		return data;
 	}
 
-	public void setData(List<ReplenishProduct> data) {
+	public void setData(List<DistributionProduct> data) {
 		this.data = data;
 	}
-	
+
 }
