e2eb12a7d00126d540f0c106664b6830c98d9df3
xutengzhou <peter.xu@yiako.com>
2018-04-08 13:59:51 +0800

    调试产生配送分拨单

---
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/ApprovalStatusNumIdAndSoNumIdUpdateResponse.java
@@ -0,0 +1,15 @@
+package com.ykcloud.soa.erp.api.scm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * 配送生成so后更新配送审批单状态并回填审批单单身so_num_id出参
+ * @author tz.x
+ *
+ * @date 2018年4月8日上午11:14:28
+ */
+public class ApprovalStatusNumIdAndSoNumIdUpdateResponse extends MessagePack {
+
+	private static final long serialVersionUID = 9135874050701174540L;
+
+}
