caa42a80f36746c65c3e93924018d76067da9ebd
shirong.chen <shirong.chen@yiako.com>
2018-04-12 14:03:44 +0800

    批次库存价格

---
+++ src/main/java/com/ykcloud/soa/erp/api/wm/model/BatchPrice.java
@@ -0,0 +1,24 @@
+package com.ykcloud.soa.erp.api.wm.model;
+
+public class BatchPrice {
+	private Double price;
+
+	private Double taxRate;
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Double getTaxRate() {
+		return taxRate;
+	}
+
+	public void setTaxRate(Double taxRate) {
+		this.taxRate = taxRate;
+	}
+
+}
