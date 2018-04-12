1dd9ff96a240ef0457471567a10a599115b9bb59
xuqian <andy.xu@yiako.com>
2018-04-12 17:47:17 +0800

    转码加工单入账接口，组装加工单入账接口

--- src/main/java/com/ykcloud/soa/erp/api/wm/service/WmWorkService.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/service/WmWorkService.java
@@ -8,9 +8,13 @@ package com.ykcloud.soa.erp.api.wm.service;
 import com.ykcloud.soa.erp.api.wm.request.OpenWorkConfirmRequest;
 import com.ykcloud.soa.erp.api.wm.request.ProductInfoAndStockGetRequest;
 import com.ykcloud.soa.erp.api.wm.request.ProductZMGetRequest;
+import com.ykcloud.soa.erp.api.wm.request.WorkChangeConfirmRequest;
+import com.ykcloud.soa.erp.api.wm.request.WorkConfirmAssembleRequest;
 import com.ykcloud.soa.erp.api.wm.response.OpenWorkConfirmResponse;
 import com.ykcloud.soa.erp.api.wm.response.ProductInfoAndStockGetResponse;
 import com.ykcloud.soa.erp.api.wm.response.ProductZMGetResponse;
+import com.ykcloud.soa.erp.api.wm.response.WorkChangeConfirmResponse;
+import com.ykcloud.soa.erp.api.wm.response.WorkConfirmAssembleResponse;
 
 public interface WmWorkService {
 	//获取商品信息+商品库存信息
@@ -21,4 +25,23 @@ public interface WmWorkService {
 
 	//拆解加工单入账
 	public OpenWorkConfirmResponse confirmOpenWork(OpenWorkConfirmRequest request);
+	
+	/**
+	 * 
+	 * @Title: confirmWorkChange  
+	 * @Description: 转码加工单入账
+	 * @param @return
+	 * @return WorkChangeConfirmResponse
+	 */
+	public WorkChangeConfirmResponse confirmWorkChange(WorkChangeConfirmRequest request);
+	
+	/**
+	 * 
+	 * @Title: confirmAssembleWork  
+	 * @Description: 组装加工单入账
+	 * @param @param request
+	 * @param @return
+	 * @return WorkConfirmAssembleResponse
+	 */
+	public WorkConfirmAssembleResponse confirmAssembleWork(WorkConfirmAssembleRequest request);
 }
