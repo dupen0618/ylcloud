51586b011cf91fdfc818e33f08b6d4678e2072e8
xutengzhou <peter.xu@yiako.com>
2018-04-14 16:07:15 +0800

    distribution so

---
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/ApprovalDtlAndApplyDtlForGenerateDistributionUpdateResponse.java
@@ -0,0 +1,15 @@
+package com.ykcloud.soa.erp.api.scm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * 
+ * @author tz.x
+ *
+ * @date 2018年4月14日上午9:07:09
+ */
+public class ApprovalDtlAndApplyDtlForGenerateDistributionUpdateResponse extends MessagePack {
+
+	private static final long serialVersionUID = 5870545387046291226L;
+
+}
