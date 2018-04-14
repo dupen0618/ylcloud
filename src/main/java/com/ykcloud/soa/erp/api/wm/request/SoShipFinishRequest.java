3150172fbd58a9c9313d109d81e16ba7368af7f6
dan <dan.wang@yiako.com>
2018-04-14 10:07:37 +0800

    so出库

---
+++ src/main/java/com/ykcloud/soa/erp/api/wm/request/SoShipFinishRequest.java
@@ -0,0 +1,38 @@
+package com.ykcloud.soa.erp.api.wm.request;
+
+import javax.validation.constraints.NotNull;
+
+import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
+
+public class SoShipFinishRequest extends AbstractRequest {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2680423974639245423L;
+	
+	@NotNull(message = "门店编号不能为空! ")
+	private Long subUnitNumId;
+	
+	@NotNull(message = "订单编号不能为空! ")
+	private String soNumId;
+
+	public Long getSubUnitNumId() {
+		return subUnitNumId;
+	}
+
+	public void setSubUnitNumId(Long subUnitNumId) {
+		this.subUnitNumId = subUnitNumId;
+	}
+
+	public String getSoNumId() {
+		return soNumId;
+	}
+
+	public void setSoNumId(String soNumId) {
+		this.soNumId = soNumId;
+	}
+	
+	
+
+}
