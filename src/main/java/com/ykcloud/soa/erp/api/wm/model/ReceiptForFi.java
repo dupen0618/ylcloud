32fdda990f255adb08e5f9d2e06938a62ea6294a
shirong.chen <shirong.chen@yiako.com>
2018-04-14 10:02:37 +0800

    model重构

--- src/main/java/com/ykcloud/soa/erp/api/wm/model/ReceiptForFi.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/model/ReceiptForFi.java
@@ -3,74 +3,72 @@ package com.ykcloud.soa.erp.api.wm.model;
 import java.io.Serializable;
 import java.util.Date;
 
-public class ReceiptForFi implements Serializable{
+public class ReceiptForFi implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	private Long series;//行号
-	
-	private Long reservedNo;//验收单号单号
-	
-	private Long tenantNumId;//租户
-	
-	private Long subUnitNumId;//门店
-	
-	private int statusNumId;//状态
-	
-	private int typeNumId;//业务类型
-	
-	private Long itemNumId;//商品
-	
-	private String divNumId;//商品中心
-	
-	private String barcode;//商品条码
-	
-	private String itemName;//商品品名
 
-	private String productOriginNumId;//产地
-	
-	private Long conversionQty;//件装单位
+	private String series;// 行号
 
-	private String conversionStyleDesc;//件装规格
+	private String reservedNo;// 验收单号单号
 
-	private Double price;//结算价格
-	
-	private Double totalAmount;//金额
-	
-	private String batchSeries;//批次行号
+	private Long tenantNumId;// 租户
 
-	private Long batchPromotionGrade;//批次促销档期
+	private Long subUnitNumId;// 门店
 
-	private int batchSupplyUnitNumId;//批次供应商
+	private Long statusNumId;// 状态
 
-	private Long batchPoPrice;//批次采购价格
-	
-	private Double taxRate;//税率
-	
-	private Long taxAmount;//税金
-	
-	private int settlementType;//结算方式
-	
-	private Long createUserId;//创建人
-	
-	private Long lastUpdateUserId;//更新人
+	private Long typeNumId;// 业务类型
 
-	private Date createDtme;//创建时间
-	
-	
+	private Long itemNumId;// 商品
 
-	public Long getSeries() {
+	private String divNumId;// 商品中心
+
+	private String barcode;// 商品条码
+
+	private String itemName;// 商品品名
+
+	private String productOriginNumId;// 产地
+
+	private Double conversionQty;// 件装单位
+
+	private String conversionStyleDesc;// 件装规格
+
+	private Double price;// 结算价格
+
+	private Double totalAmount;// 金额
+
+	private String batchSeries;// 批次行号
+
+	private String batchPromotionGrade;// 批次促销档期
+
+	private Long batchSupplyUnitNumId;// 批次供应商
+
+	private Double batchPoPrice;// 批次采购价格
+
+	private Double taxRate;// 税率
+
+	private Double taxAmount;// 税金
+
+	private Long settlementType;// 结算方式
+
+	private Long createUserId;// 创建人
+
+	private Long lastUpdateUserId;// 更新人
+
+	private Date createDtme;// 创建时间
+
+	public String getSeries() {
 		return series;
 	}
 
-	public void setSeries(Long series) {
+	public void setSeries(String series) {
 		this.series = series;
 	}
 
-	public Long getReservedNo() {
+	public String getReservedNo() {
 		return reservedNo;
 	}
 
-	public void setReservedNo(Long reservedNo) {
+	public void setReservedNo(String reservedNo) {
 		this.reservedNo = reservedNo;
 	}
 
@@ -90,19 +88,19 @@ public class ReceiptForFi implements Serializable{
 		this.subUnitNumId = subUnitNumId;
 	}
 
-	public int getStatusNumId() {
+	public Long getStatusNumId() {
 		return statusNumId;
 	}
 
-	public void setStatusNumId(int statusNumId) {
+	public void setStatusNumId(Long statusNumId) {
 		this.statusNumId = statusNumId;
 	}
 
-	public int getTypeNumId() {
+	public Long getTypeNumId() {
 		return typeNumId;
 	}
 
-	public void setTypeNumId(int typeNumId) {
+	public void setTypeNumId(Long typeNumId) {
 		this.typeNumId = typeNumId;
 	}
 
@@ -146,11 +144,11 @@ public class ReceiptForFi implements Serializable{
 		this.productOriginNumId = productOriginNumId;
 	}
 
-	public Long getConversionQty() {
+	public Double getConversionQty() {
 		return conversionQty;
 	}
 
-	public void setConversionQty(Long conversionQty) {
+	public void setConversionQty(Double conversionQty) {
 		this.conversionQty = conversionQty;
 	}
 
@@ -186,27 +184,27 @@ public class ReceiptForFi implements Serializable{
 		this.batchSeries = batchSeries;
 	}
 
-	public Long getBatchPromotionGrade() {
+	public String getBatchPromotionGrade() {
 		return batchPromotionGrade;
 	}
 
-	public void setBatchPromotionGrade(Long batchPromotionGrade) {
+	public void setBatchPromotionGrade(String batchPromotionGrade) {
 		this.batchPromotionGrade = batchPromotionGrade;
 	}
 
-	public int getBatchSupplyUnitNumId() {
+	public Long getBatchSupplyUnitNumId() {
 		return batchSupplyUnitNumId;
 	}
 
-	public void setBatchSupplyUnitNumId(int batchSupplyUnitNumId) {
+	public void setBatchSupplyUnitNumId(Long batchSupplyUnitNumId) {
 		this.batchSupplyUnitNumId = batchSupplyUnitNumId;
 	}
 
-	public Long getBatchPoPrice() {
+	public Double getBatchPoPrice() {
 		return batchPoPrice;
 	}
 
-	public void setBatchPoPrice(Long batchPoPrice) {
+	public void setBatchPoPrice(Double batchPoPrice) {
 		this.batchPoPrice = batchPoPrice;
 	}
 
@@ -218,19 +216,19 @@ public class ReceiptForFi implements Serializable{
 		this.taxRate = taxRate;
 	}
 
-	public Long getTaxAmount() {
+	public Double getTaxAmount() {
 		return taxAmount;
 	}
 
-	public void setTaxAmount(Long taxAmount) {
+	public void setTaxAmount(Double taxAmount) {
 		this.taxAmount = taxAmount;
 	}
 
-	public int getSettlementType() {
+	public Long getSettlementType() {
 		return settlementType;
 	}
 
-	public void setSettlementType(int settlementType) {
+	public void setSettlementType(Long settlementType) {
 		this.settlementType = settlementType;
 	}
 
@@ -257,6 +255,5 @@ public class ReceiptForFi implements Serializable{
 	public void setCreateDtme(Date createDtme) {
 		this.createDtme = createDtme;
 	}
-	
-	
+
 }
--- src/main/java/com/ykcloud/soa/erp/api/wm/model/ScmAccInvOwnStk.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/model/ScmAccInvOwnStk.java
@@ -4,233 +4,233 @@ import java.util.Date;
 
 /**
  * @Author Hewei
- * @Date 2018/3/25 21:27
- * 此类特殊，需要与Entity一样
+ * @Date 2018/3/25 21:27 此类特殊，需要与Entity一样
  */
 public class ScmAccInvOwnStk {
-    private Long TENANT_NUM_ID;
-    private Long DIV_NUM_ID;
-    private Long STORAGE_NUM_ID;
-    private Long LOC_PTY_NUM_ID;
-    private Long ITEM_NUM_ID;
-    private Long PHYSIC_QTY;
-    private Long RESERVED_QTY;
-    private Long LOCK_QTY;
-    private Long KEEP_QTY;
-    private Long AVAL_QTY;
-    private String CANCELSIGN;
-    private String INSERTDATA;
-    private String UPDATEDATA;
-    private Long VIRTUAL_QTY;
-    private Long FACT_VIRTUAL_QTY;
-    private Long SUB_UNIT_NUM_ID;
-    private Long SAMPLE_QTY;
-    private Long DEFECTIVE_QTY;
-    private Long SERIES;
-    private Long CREATE_USER_ID;
-    private Long LAST_UPDATE_USER_ID;
-    private Date CREATE_DTME;
-    private Date LAST_UPDTME;
-    private Long CARRY_SIGN;
-    private Long DATA_SIGN;
+	private Long TENANT_NUM_ID;
+	private Long DIV_NUM_ID;
+	private Long STORAGE_NUM_ID;
+	private Long LOC_PTY_NUM_ID;
+	private Long ITEM_NUM_ID;
+	private Double PHYSIC_QTY;
+	private Double RESERVED_QTY;
+	private Double LOCK_QTY;
+	private Double KEEP_QTY;
+	private Double AVAL_QTY;
+	private String CANCELSIGN;
+	private String INSERTDATA;
+	private String UPDATEDATA;
+	private Double VIRTUAL_QTY;
+	private Double FACT_VIRTUAL_QTY;
+	private Long SUB_UNIT_NUM_ID;
+	private Double SAMPLE_QTY;
+	private Double DEFECTIVE_QTY;
+	private String SERIES;
+	private Long CREATE_USER_ID;
+	private Long LAST_UPDATE_USER_ID;
+	private Date CREATE_DTME;
+	private Date LAST_UPDTME;
+	private Long CARRY_SIGN;
+	private Long DATA_SIGN;
 
-    public Long getTENANT_NUM_ID() {
-        return TENANT_NUM_ID;
-    }
+	public Long getTENANT_NUM_ID() {
+		return TENANT_NUM_ID;
+	}
 
-    public void setTENANT_NUM_ID(Long TENANT_NUM_ID) {
-        this.TENANT_NUM_ID = TENANT_NUM_ID;
-    }
+	public void setTENANT_NUM_ID(Long tENANT_NUM_ID) {
+		TENANT_NUM_ID = tENANT_NUM_ID;
+	}
 
-    public Long getDIV_NUM_ID() {
-        return DIV_NUM_ID;
-    }
+	public Long getDIV_NUM_ID() {
+		return DIV_NUM_ID;
+	}
 
-    public void setDIV_NUM_ID(Long DIV_NUM_ID) {
-        this.DIV_NUM_ID = DIV_NUM_ID;
-    }
+	public void setDIV_NUM_ID(Long dIV_NUM_ID) {
+		DIV_NUM_ID = dIV_NUM_ID;
+	}
 
-    public Long getSTORAGE_NUM_ID() {
-        return STORAGE_NUM_ID;
-    }
+	public Long getSTORAGE_NUM_ID() {
+		return STORAGE_NUM_ID;
+	}
 
-    public void setSTORAGE_NUM_ID(Long STORAGE_NUM_ID) {
-        this.STORAGE_NUM_ID = STORAGE_NUM_ID;
-    }
+	public void setSTORAGE_NUM_ID(Long sTORAGE_NUM_ID) {
+		STORAGE_NUM_ID = sTORAGE_NUM_ID;
+	}
 
-    public Long getLOC_PTY_NUM_ID() {
-        return LOC_PTY_NUM_ID;
-    }
+	public Long getLOC_PTY_NUM_ID() {
+		return LOC_PTY_NUM_ID;
+	}
 
-    public void setLOC_PTY_NUM_ID(Long LOC_PTY_NUM_ID) {
-        this.LOC_PTY_NUM_ID = LOC_PTY_NUM_ID;
-    }
+	public void setLOC_PTY_NUM_ID(Long lOC_PTY_NUM_ID) {
+		LOC_PTY_NUM_ID = lOC_PTY_NUM_ID;
+	}
 
-    public Long getITEM_NUM_ID() {
-        return ITEM_NUM_ID;
-    }
+	public Long getITEM_NUM_ID() {
+		return ITEM_NUM_ID;
+	}
 
-    public void setITEM_NUM_ID(Long ITEM_NUM_ID) {
-        this.ITEM_NUM_ID = ITEM_NUM_ID;
-    }
+	public void setITEM_NUM_ID(Long iTEM_NUM_ID) {
+		ITEM_NUM_ID = iTEM_NUM_ID;
+	}
 
-    public Long getPHYSIC_QTY() {
-        return PHYSIC_QTY;
-    }
+	public Double getPHYSIC_QTY() {
+		return PHYSIC_QTY;
+	}
 
-    public void setPHYSIC_QTY(Long PHYSIC_QTY) {
-        this.PHYSIC_QTY = PHYSIC_QTY;
-    }
+	public void setPHYSIC_QTY(Double pHYSIC_QTY) {
+		PHYSIC_QTY = pHYSIC_QTY;
+	}
 
-    public Long getRESERVED_QTY() {
-        return RESERVED_QTY;
-    }
+	public Double getRESERVED_QTY() {
+		return RESERVED_QTY;
+	}
 
-    public void setRESERVED_QTY(Long RESERVED_QTY) {
-        this.RESERVED_QTY = RESERVED_QTY;
-    }
+	public void setRESERVED_QTY(Double rESERVED_QTY) {
+		RESERVED_QTY = rESERVED_QTY;
+	}
 
-    public Long getLOCK_QTY() {
-        return LOCK_QTY;
-    }
+	public Double getLOCK_QTY() {
+		return LOCK_QTY;
+	}
 
-    public void setLOCK_QTY(Long LOCK_QTY) {
-        this.LOCK_QTY = LOCK_QTY;
-    }
+	public void setLOCK_QTY(Double lOCK_QTY) {
+		LOCK_QTY = lOCK_QTY;
+	}
 
-    public Long getKEEP_QTY() {
-        return KEEP_QTY;
-    }
+	public Double getKEEP_QTY() {
+		return KEEP_QTY;
+	}
 
-    public void setKEEP_QTY(Long KEEP_QTY) {
-        this.KEEP_QTY = KEEP_QTY;
-    }
+	public void setKEEP_QTY(Double kEEP_QTY) {
+		KEEP_QTY = kEEP_QTY;
+	}
 
-    public Long getAVAL_QTY() {
-        return AVAL_QTY;
-    }
+	public Double getAVAL_QTY() {
+		return AVAL_QTY;
+	}
 
-    public void setAVAL_QTY(Long AVAL_QTY) {
-        this.AVAL_QTY = AVAL_QTY;
-    }
+	public void setAVAL_QTY(Double aVAL_QTY) {
+		AVAL_QTY = aVAL_QTY;
+	}
 
-    public String getCANCELSIGN() {
-        return CANCELSIGN;
-    }
+	public String getCANCELSIGN() {
+		return CANCELSIGN;
+	}
 
-    public void setCANCELSIGN(String CANCELSIGN) {
-        this.CANCELSIGN = CANCELSIGN;
-    }
+	public void setCANCELSIGN(String cANCELSIGN) {
+		CANCELSIGN = cANCELSIGN;
+	}
 
-    public String getINSERTDATA() {
-        return INSERTDATA;
-    }
+	public String getINSERTDATA() {
+		return INSERTDATA;
+	}
 
-    public void setINSERTDATA(String INSERTDATA) {
-        this.INSERTDATA = INSERTDATA;
-    }
+	public void setINSERTDATA(String iNSERTDATA) {
+		INSERTDATA = iNSERTDATA;
+	}
 
-    public String getUPDATEDATA() {
-        return UPDATEDATA;
-    }
+	public String getUPDATEDATA() {
+		return UPDATEDATA;
+	}
 
-    public void setUPDATEDATA(String UPDATEDATA) {
-        this.UPDATEDATA = UPDATEDATA;
-    }
+	public void setUPDATEDATA(String uPDATEDATA) {
+		UPDATEDATA = uPDATEDATA;
+	}
 
-    public Long getVIRTUAL_QTY() {
-        return VIRTUAL_QTY;
-    }
+	public Double getVIRTUAL_QTY() {
+		return VIRTUAL_QTY;
+	}
 
-    public void setVIRTUAL_QTY(Long VIRTUAL_QTY) {
-        this.VIRTUAL_QTY = VIRTUAL_QTY;
-    }
+	public void setVIRTUAL_QTY(Double vIRTUAL_QTY) {
+		VIRTUAL_QTY = vIRTUAL_QTY;
+	}
 
-    public Long getFACT_VIRTUAL_QTY() {
-        return FACT_VIRTUAL_QTY;
-    }
+	public Double getFACT_VIRTUAL_QTY() {
+		return FACT_VIRTUAL_QTY;
+	}
 
-    public void setFACT_VIRTUAL_QTY(Long FACT_VIRTUAL_QTY) {
-        this.FACT_VIRTUAL_QTY = FACT_VIRTUAL_QTY;
-    }
+	public void setFACT_VIRTUAL_QTY(Double fACT_VIRTUAL_QTY) {
+		FACT_VIRTUAL_QTY = fACT_VIRTUAL_QTY;
+	}
 
-    public Long getSUB_UNIT_NUM_ID() {
-        return SUB_UNIT_NUM_ID;
-    }
+	public Long getSUB_UNIT_NUM_ID() {
+		return SUB_UNIT_NUM_ID;
+	}
 
-    public void setSUB_UNIT_NUM_ID(Long SUB_UNIT_NUM_ID) {
-        this.SUB_UNIT_NUM_ID = SUB_UNIT_NUM_ID;
-    }
+	public void setSUB_UNIT_NUM_ID(Long sUB_UNIT_NUM_ID) {
+		SUB_UNIT_NUM_ID = sUB_UNIT_NUM_ID;
+	}
 
-    public Long getSAMPLE_QTY() {
-        return SAMPLE_QTY;
-    }
+	public Double getSAMPLE_QTY() {
+		return SAMPLE_QTY;
+	}
 
-    public void setSAMPLE_QTY(Long SAMPLE_QTY) {
-        this.SAMPLE_QTY = SAMPLE_QTY;
-    }
+	public void setSAMPLE_QTY(Double sAMPLE_QTY) {
+		SAMPLE_QTY = sAMPLE_QTY;
+	}
 
-    public Long getDEFECTIVE_QTY() {
-        return DEFECTIVE_QTY;
-    }
+	public Double getDEFECTIVE_QTY() {
+		return DEFECTIVE_QTY;
+	}
 
-    public void setDEFECTIVE_QTY(Long DEFECTIVE_QTY) {
-        this.DEFECTIVE_QTY = DEFECTIVE_QTY;
-    }
+	public void setDEFECTIVE_QTY(Double dEFECTIVE_QTY) {
+		DEFECTIVE_QTY = dEFECTIVE_QTY;
+	}
 
-    public Long getSERIES() {
-        return SERIES;
-    }
+	public String getSERIES() {
+		return SERIES;
+	}
 
-    public void setSERIES(Long SERIES) {
-        this.SERIES = SERIES;
-    }
+	public void setSERIES(String sERIES) {
+		SERIES = sERIES;
+	}
 
-    public Long getCREATE_USER_ID() {
-        return CREATE_USER_ID;
-    }
+	public Long getCREATE_USER_ID() {
+		return CREATE_USER_ID;
+	}
 
-    public void setCREATE_USER_ID(Long CREATE_USER_ID) {
-        this.CREATE_USER_ID = CREATE_USER_ID;
-    }
+	public void setCREATE_USER_ID(Long cREATE_USER_ID) {
+		CREATE_USER_ID = cREATE_USER_ID;
+	}
 
-    public Long getLAST_UPDATE_USER_ID() {
-        return LAST_UPDATE_USER_ID;
-    }
+	public Long getLAST_UPDATE_USER_ID() {
+		return LAST_UPDATE_USER_ID;
+	}
 
-    public void setLAST_UPDATE_USER_ID(Long LAST_UPDATE_USER_ID) {
-        this.LAST_UPDATE_USER_ID = LAST_UPDATE_USER_ID;
-    }
+	public void setLAST_UPDATE_USER_ID(Long lAST_UPDATE_USER_ID) {
+		LAST_UPDATE_USER_ID = lAST_UPDATE_USER_ID;
+	}
 
-    public Date getCREATE_DTME() {
-        return CREATE_DTME;
-    }
+	public Date getCREATE_DTME() {
+		return CREATE_DTME;
+	}
 
-    public void setCREATE_DTME(Date CREATE_DTME) {
-        this.CREATE_DTME = CREATE_DTME;
-    }
+	public void setCREATE_DTME(Date cREATE_DTME) {
+		CREATE_DTME = cREATE_DTME;
+	}
 
-    public Date getLAST_UPDTME() {
-        return LAST_UPDTME;
-    }
+	public Date getLAST_UPDTME() {
+		return LAST_UPDTME;
+	}
 
-    public void setLAST_UPDTME(Date LAST_UPDTME) {
-        this.LAST_UPDTME = LAST_UPDTME;
-    }
+	public void setLAST_UPDTME(Date lAST_UPDTME) {
+		LAST_UPDTME = lAST_UPDTME;
+	}
 
-    public Long getCARRY_SIGN() {
-        return CARRY_SIGN;
-    }
+	public Long getCARRY_SIGN() {
+		return CARRY_SIGN;
+	}
 
-    public void setCARRY_SIGN(Long CARRY_SIGN) {
-        this.CARRY_SIGN = CARRY_SIGN;
-    }
+	public void setCARRY_SIGN(Long cARRY_SIGN) {
+		CARRY_SIGN = cARRY_SIGN;
+	}
 
-    public Long getDATA_SIGN() {
-        return DATA_SIGN;
-    }
+	public Long getDATA_SIGN() {
+		return DATA_SIGN;
+	}
 
-    public void setDATA_SIGN(Long DATA_SIGN) {
-        this.DATA_SIGN = DATA_SIGN;
-    }
+	public void setDATA_SIGN(Long dATA_SIGN) {
+		DATA_SIGN = dATA_SIGN;
+	}
+
 }
