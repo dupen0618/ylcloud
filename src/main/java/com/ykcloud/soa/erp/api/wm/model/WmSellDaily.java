aef515f7fe11007fed97f12987f2872b85314513
潘小玲 <ali.pan@yiako.com>
2018-04-16 15:32:12 +0800

    序列化

--- src/main/java/com/ykcloud/soa/erp/api/wm/model/WmSellDaily.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/model/WmSellDaily.java
@@ -1,8 +1,9 @@
 package com.ykcloud.soa.erp.api.wm.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class WmSellDaily {
+public class WmSellDaily  implements Serializable {
 
     private static final long serialVersionUID = 1L;
     /**
