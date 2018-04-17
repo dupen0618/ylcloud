8a3fb910a6c981c892ed0ae18e1f4b5f2d5839fb
xuqian <andy.xu@yiako.com>
2018-04-17 17:30:37 +0800

    损溢单入账

---
+++ src/main/java/com/ykcloud/soa/erp/api/wm/response/LossWorkconfirmResponse.java
@@ -0,0 +1,8 @@
+package com.ykcloud.soa.erp.api.wm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+public class LossWorkconfirmResponse extends MessagePack {
+	private static final long serialVersionUID = 1L;
+
+}
