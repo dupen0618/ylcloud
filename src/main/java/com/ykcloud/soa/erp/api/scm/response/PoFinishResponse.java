6ce638734a9f070bc02adc612aa1cc365dd16edd
hank.zhu <hank.zhu@yiako.com>
2018-04-18 13:51:07 +0800

    ScmPoService add finishPo(),change updatePoDtlRecQ

---
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/PoFinishResponse.java
@@ -0,0 +1,12 @@
+package com.ykcloud.soa.erp.api.scm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+*Po完成方法响应类
+@author hank.zhu
+@date 2018年4月18日 上午11:31:13 新建
+**/
+public class PoFinishResponse extends MessagePack {
+	private static final long serialVersionUID = -445429049714728319L;
+}
