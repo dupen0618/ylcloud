687a6bcafb97c467049b49f983f710dcdf7f09f9
xuqian <andy.xu@yiako.com>
2018-04-18 17:51:35 +0800

    修改request

--- src/main/java/com/ykcloud/soa/erp/api/fi/request/LossAmount1AndFinalCostUpdateRequest.java
+++ src/main/java/com/ykcloud/soa/erp/api/fi/request/LossAmount1AndFinalCostUpdateRequest.java
@@ -1,7 +1,5 @@
 package com.ykcloud.soa.erp.api.fi.request;
 
-import java.util.Date;
-
 import javax.validation.constraints.NotNull;
 
 import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
@@ -18,12 +16,23 @@ public class LossAmount1AndFinalCostUpdateRequest extends AbstractRequest {
 	@NotNull(message = "物理仓库不能为空！")
 	private Long physicalNumId;
 
-	@NotNull(message = "结算日期")
+	@NotNull(message = "损耗金额不能为空！")
 	private Double losscost;
 	
-	public interface lossType{
-		public String loss = "0";
-		public String overFlow = "1";
+	@NotNull(message = "损耗类型不能为空！")
+	private String lossType;
+
+	public static interface lossType {
+		public String loss = "0";// 报损
+		public String overFlow = "1";// 报溢
+	}
+
+	public Double getLosscost() {
+		return losscost;
+	}
+
+	public void setLosscost(Double losscost) {
+		this.losscost = losscost;
 	}
 
 	public Long getSubUnitNumId() {
@@ -50,4 +59,12 @@ public class LossAmount1AndFinalCostUpdateRequest extends AbstractRequest {
 		this.physicalNumId = physicalNumId;
 	}
 
+	public String getLossType() {
+		return lossType;
+	}
+
+	public void setLossType(String lossType) {
+		this.lossType = lossType;
+	}
+	
 }
