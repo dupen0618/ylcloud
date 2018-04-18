786e8e18e79a0bf61f86de248456d0329fc0238b
aiden.song <aiden.song@yiako.com>
2018-04-18 09:28:52 +0800

    损益单和盈亏单

---
+++ src/main/java/com/ykcloud/soa/erp/api/wm/request/StockadJustSeriesGetRequest.java
@@ -0,0 +1,39 @@
+package com.ykcloud.soa.erp.api.wm.request;
+
+import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
+
+public class StockadJustSeriesGetRequest extends AbstractRequest {
+
+	private static final long serialVersionUID = -7777920135123732638L;
+
+	 private  String batchSeries;
+	    private  Long itemUumId;
+	    private String lossNumId;
+	  
+	    public String getBatchSeries() {
+			return batchSeries;
+		}
+
+		public void setBatchSeries(String batchSeries) {
+			this.batchSeries = batchSeries;
+		}
+
+		public Long getItemUumId() {
+	        return itemUumId;
+	    }
+
+	    public void setItemUumId(Long itemUumId) {
+	        this.itemUumId = itemUumId;
+	    }
+
+		public String getLossNumId() {
+			return lossNumId;
+		}
+
+		public void setLossNumId(String lossNumId) {
+			this.lossNumId = lossNumId;
+		}
+
+		
+
+}
--- src/main/java/com/ykcloud/soa/erp/api/wm/service/WmReceiptForFiService.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/service/WmReceiptForFiService.java
@@ -2,13 +2,29 @@ package com.ykcloud.soa.erp.api.wm.service;
 
 
 import com.ykcloud.soa.erp.api.wm.request.BalanceDtlByDeductionSelectRequest;
+import com.ykcloud.soa.erp.api.wm.request.LoseDtlDataGetRequest;
+import com.ykcloud.soa.erp.api.wm.request.LossDtlByBalanceSelectRequest;
+import com.ykcloud.soa.erp.api.wm.request.LossSeriesGetRequest;
+import com.ykcloud.soa.erp.api.wm.request.LossStatusUpdateRequest;
 import com.ykcloud.soa.erp.api.wm.request.ReceiptForFiGetRequest;
 import com.ykcloud.soa.erp.api.wm.request.ReceiptSeriesGetRequest;
 import com.ykcloud.soa.erp.api.wm.request.ReceiptStatusUpdateRequest;
+import com.ykcloud.soa.erp.api.wm.request.StockadJustDataGetRequest;
+import com.ykcloud.soa.erp.api.wm.request.StockadJustSelectRequest;
+import com.ykcloud.soa.erp.api.wm.request.StockadJustSeriesGetRequest;
+import com.ykcloud.soa.erp.api.wm.request.StockadJustStatusGetRequest;
 import com.ykcloud.soa.erp.api.wm.response.BalanceDtlByDeductionSelectResponse;
+import com.ykcloud.soa.erp.api.wm.response.LossDtlByBalanceSelectResponse;
+import com.ykcloud.soa.erp.api.wm.response.LossDtlDataGetResponse;
+import com.ykcloud.soa.erp.api.wm.response.LossSeriesGetResponse;
+import com.ykcloud.soa.erp.api.wm.response.LossStatusUpdateResponse;
 import com.ykcloud.soa.erp.api.wm.response.ReceiptForFiGetResponse;
 import com.ykcloud.soa.erp.api.wm.response.ReceiptSeriesGetResponse;
 import com.ykcloud.soa.erp.api.wm.response.ReceiptStatusUpdateResponse;
+import com.ykcloud.soa.erp.api.wm.response.StockadJustDataGetResponse;
+import com.ykcloud.soa.erp.api.wm.response.StockadJustSelectResponse;
+import com.ykcloud.soa.erp.api.wm.response.StockadJustSeriesGetResponse;
+import com.ykcloud.soa.erp.api.wm.response.StockadJustStatusGetResponse;
 
 /**
  * @Description:
@@ -28,5 +44,31 @@ public interface WmReceiptForFiService {
 	
 	//选择指定验收入库单明细项目生成结算明细
 	public BalanceDtlByDeductionSelectResponse selectBalanceDtlByDeduction(BalanceDtlByDeductionSelectRequest request);
+	
+	
+	//损益单获取数据
+	public LossDtlDataGetResponse getLoseDtlData(LoseDtlDataGetRequest request);
+	
+	//改变损益单结算后的状态
+	public LossStatusUpdateResponse updateLossStatus(LossStatusUpdateRequest request);
+	
+	//得到损益单行号
+	public LossSeriesGetResponse getLossSeries(LossSeriesGetRequest request);
+	
+	//选择指定损益单明细生成结算单
+	public LossDtlByBalanceSelectResponse selectLossDtlByBalance(LossDtlByBalanceSelectRequest request);
+	
+	
+	//盈亏单获取数据
+	public StockadJustDataGetResponse getStockadJustData(StockadJustDataGetRequest request);
+	
+	//改变盈亏单结算后的状态
+	public StockadJustStatusGetResponse getStockadJustStatus(StockadJustStatusGetRequest request);
+	
+	//获得盈亏单行号
+	public StockadJustSeriesGetResponse getStockadJustSeries(StockadJustSeriesGetRequest request);
+	
+	//选择指定的盈亏单明细生成结算单
+	public StockadJustSelectResponse selectStockadJust(StockadJustSelectRequest request);
 	
 }
