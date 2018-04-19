a15e30f3d9368d9492f9653dee20e9a9901dc652
shirong.chen <shirong.chen@yiako.com>
2018-04-19 13:46:38 +0800

    退货实际数量回写

---
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/ReturnApprovalOrderActualQtyUpdateResponse.java
@@ -0,0 +1,15 @@
+package com.ykcloud.soa.erp.api.scm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * 更新退货审批单单身so_num_id入参
+ * @author tz.x
+ *
+ * @date 2018年4月10日下午7:05:24
+ */
+public class ReturnApprovalOrderActualQtyUpdateResponse extends MessagePack {
+
+	private static final long serialVersionUID = -2664236631888518538L;
+
+}
