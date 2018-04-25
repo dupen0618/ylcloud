9675d5188258dd4e72079378f3dec5efd4765a80
zhaokang <fred.zhao@yiako.com>
2018-04-25 11:00:27 +0800

    4.26合并代码

---
+++ src/main/java/com/ykcloud/soa/erp/api/so/response/SoTmlTranTypeUpdateResponse.java
@@ -0,0 +1,11 @@
+package com.ykcloud.soa.erp.api.so.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * @Author: fred.zhao
+ * @Description:
+ * @Date: Created in 10:18:2018/4/25
+ */
+public class SoTmlTranTypeUpdateResponse extends MessagePack {
+}
