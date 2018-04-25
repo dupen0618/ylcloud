8db12c739b87337135bfbe58abb858c3cf812b32
damon.he@yiako.com <damon.he@yiako.com>
2018-04-25 12:09:56 +0800

    代销商品款，增加盘盈盘亏单

--- src/main/java/com/ykcloud/soa/erp/api/wm/response/InAccountByReceiptResponse.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/response/InAccountByReceiptResponse.java
@@ -8,14 +8,4 @@ import com.gb.soa.omp.ccommon.api.response.MessagePack;
  */
 public class InAccountByReceiptResponse extends MessagePack {
     private static final long serialVersionUID = 1L;
-
-    private Exception e;
-
-    public Exception getE() {
-        return e;
-    }
-
-    public void setE(Exception e) {
-        this.e = e;
-    }
 }
