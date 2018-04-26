5f1e2e830ff5c306ae7c264119f4befe3556b01a
damon.he@yiako.com <damon.he@yiako.com>
2018-04-26 11:30:54 +0800

    临时提交

---
+++ src/main/java/com/ykcloud/soa/erp/api/wm/request/InAccountByWorkRawRequest.java
@@ -0,0 +1,54 @@
+package com.ykcloud.soa.erp.api.wm.request;
+
+import com.gb.soa.omp.ccommon.api.request.AbstractUserSessionRequest;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @Author Hewei
+ * @Date 2018/4/25 19:02
+ */
+public class InAccountByWorkRawRequest extends AbstractUserSessionRequest {
+    private static final long serialVersionUID = 1L;
+
+    @NotNull(message = "订单编号不能为空！")
+    String reservedNo;
+    @NotNull(message = "单据类型不能为空！")
+    Long typeNumId;
+    @NotNull(message="门店编号不能为空")
+    Long subUnitNumId;
+
+    Long balaFuncId;
+
+    public String getReservedNo() {
+        return reservedNo;
+    }
+
+    public void setReservedNo(String reservedNo) {
+        this.reservedNo = reservedNo;
+    }
+
+    public Long getTypeNumId() {
+        return typeNumId;
+    }
+
+    public void setTypeNumId(Long typeNumId) {
+        this.typeNumId = typeNumId;
+    }
+
+    public Long getBalaFuncId() {
+        return balaFuncId;
+    }
+
+    public void setBalaFuncId(Long balaFuncId) {
+        this.balaFuncId = balaFuncId;
+    }
+
+    public Long getSubUnitNumId() {
+        return subUnitNumId;
+    }
+
+    public void setSubUnitNumId(Long subUnitNumId) {
+        this.subUnitNumId = subUnitNumId;
+    }
+}
---
+++ src/main/java/com/ykcloud/soa/erp/api/wm/response/InAccountByLossResponse.java
@@ -0,0 +1,10 @@
+package com.ykcloud.soa.erp.api.wm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * @Author Hewei
+ * @Date 2018/4/25 19:09
+ */
+public class InAccountByLossResponse extends MessagePack {
+}
