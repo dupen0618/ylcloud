17c511f69cc3e4c0b757d11b716beeb505252fb3
shirong.chen <shirong.chen@yiako.com>
2018-05-01 11:15:55 +0800

    在途数量接口重构

--- src/main/java/com/ykcloud/soa/erp/api/scm/request/InTransitDepositRequest.java
+++ src/main/java/com/ykcloud/soa/erp/api/scm/request/InTransitDepositRequest.java
@@ -1,14 +1,11 @@
 package com.ykcloud.soa.erp.api.scm.request;
 
 import java.util.Date;
-import java.util.List;
-
 import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Range;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gb.soa.omp.ccommon.api.annotation.ApiField;
 import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
-import com.ykcloud.soa.erp.api.scm.model.InTransitWriteoffBeforeDeposit;
 
 public class InTransitDepositRequest extends AbstractRequest {
 	private static final long serialVersionUID = 5118059696054727934L;
@@ -24,12 +21,6 @@ public class InTransitDepositRequest extends AbstractRequest {
 	@ApiField(description = "1:补货申请单 2:订货审批单 3:配送审批单 4:采购单  5:验收单  6:执行订单so")
 	private Long billType;
 
-	// @NotNull(message = "单据类型不能为空！")
-	// @ApiField(description = "100:补货申请单 200:采购单
-	// 3XX:执行订单so(301：直通分拨到店（总仓），302：直通分拨到仓，303：配送到店，304：配送分拨到仓，305：配送分拨到店，309：直送退供应商，310：直通退供应商
-	// 311：配送退总仓，315：直通分拨到店（分仓）")
-	// private Long billType;
-
 	@NotNull(message = "单据编号不能为空！")
 	private String reservedNo;
 
@@ -41,8 +32,6 @@ public class InTransitDepositRequest extends AbstractRequest {
 	@ApiField(description = "1:入账 2:销账")
 	@Range(min = 1L, max = 2L, message = "1:入账 2:销账")
 	private Long depositSign;
-
-	private List<InTransitWriteoffBeforeDeposit> writeoffList;
 
 	public Long getSubUnitNumId() {
 		return subUnitNumId;
@@ -90,20 +79,6 @@ public class InTransitDepositRequest extends AbstractRequest {
 
 	public void setTypeNumId(Long typeNumId) {
 		this.typeNumId = typeNumId;
-	}
-
-	/*
-	 * public Long getBillType() { return billType; }
-	 * 
-	 * public void setBillType(Long billType) { this.billType = billType; }
-	 */
-
-	public List<InTransitWriteoffBeforeDeposit> getWriteoffList() {
-		return writeoffList;
-	}
-
-	public void setWriteoffList(List<InTransitWriteoffBeforeDeposit> writeoffList) {
-		this.writeoffList = writeoffList;
 	}
 
 }
