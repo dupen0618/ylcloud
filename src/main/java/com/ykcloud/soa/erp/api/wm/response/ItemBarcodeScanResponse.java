df807a174d6869832ebcc728f56573c6bab7367b
henry.wang <henry.wang@yiako.com>
2018-05-02 10:28:15 +0800

    根据前端要求，修改WM024、WM025、WM026以数组形式返回

--- src/main/java/com/ykcloud/soa/erp/api/wm/response/ItemBarcodeScanResponse.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/response/ItemBarcodeScanResponse.java
@@ -2,6 +2,7 @@ package com.ykcloud.soa.erp.api.wm.response;
 
 import com.gb.soa.omp.ccommon.api.response.MessagePack;
 import com.ykcloud.soa.erp.api.wm.model.ProductInfo;
+import java.util.List;
 
 /**
  * @Description: 扫码商品条码返回商品信息的出参
@@ -12,13 +13,13 @@ public class ItemBarcodeScanResponse extends MessagePack {
 
     private static final long serialVersionUID = 1L;
 
-    private ProductInfo productInfo;
+    private List<ProductInfo> productInfos;
 
-    public ProductInfo getProductInfo() {
-        return productInfo;
+    public List<ProductInfo> getProductInfos() {
+        return productInfos;
     }
 
-    public void setProductInfo(ProductInfo productInfo) {
-        this.productInfo = productInfo;
+    public void setProductInfos(List<ProductInfo> productInfos) {
+        this.productInfos = productInfos;
     }
 }
