66b30d81d85343fd4d7525d085515b038502daa4
shirong.chen <shirong.chen@yiako.com>
2018-05-04 16:22:34 +0800

    退货单头栏位缺失

--- src/main/java/com/ykcloud/soa/erp/api/so/model/ReturnApplyDtl.java
+++ src/main/java/com/ykcloud/soa/erp/api/so/model/ReturnApplyDtl.java
@@ -9,236 +9,246 @@ import java.io.Serializable;
  */
 public class ReturnApplyDtl implements Serializable {
 
-    private static final long serialVersionUID = -2305750048515780399L;
+	private static final long serialVersionUID = -2305750048515780399L;
 
-    private String series;//序号
+	private String series;// 序号
 
-    private String  applyNumId;//申请单号
+	private String applyNumId;// 申请单号
 
-    private Long itemNumId;//商品编号
+	private Long itemNumId;// 商品编号
 
-    private Long divNumId;//商品种类
+	private String itemid;
 
-    private String barcode;//条码
+	private Long divNumId;// 商品种类
 
-    private String itemName;//商品名称
+	private String barcode;// 条码
 
-    private Double lockQty;//锁库数量
+	private String itemName;// 商品名称
 
-    private Double stockQty;//库存数量
+	private Double lockQty;// 锁库数量
 
-    private Long productOriginNumId;//产地
+	private Double stockQty;// 库存数量
 
-    private Double conversionQty;//件装数
+	private Long productOriginNumId;// 产地
 
-    private Long conversionUnitNumId;//件装单位
+	private Double conversionQty;// 件装数
 
-    private String conversionStyleDesc;//件装规格
+	private Long conversionUnitNumId;// 件装单位
 
-    private Double packageQty;//件数
+	private String conversionStyleDesc;// 件装规格
 
-    private Double qty;//数量
+	private Double packageQty;// 件数
 
-    private Double pmtQty;//赠品数量
+	private Double qty;// 数量
 
-    private Long pmtMaterial;//赠品辅料
+	private Double pmtQty;// 赠品数量
 
-    private Double tradePrice;//价格
+	private Long pmtMaterial;// 赠品辅料
 
-    private Double tradeAmount;//金额
+	private Double tradePrice;// 价格
 
-    private Double taxRate;//税率
+	private Double tradeAmount;// 金额
 
-    private Double taxAmount;//税额
+	private Double taxRate;// 税率
 
-    private Double orderQty;//生成订单数量
+	private Double taxAmount;// 税额
 
-    private Long returnReasonId;//退货原因
+	private Double orderQty;// 生成订单数量
 
-    private String returnDesc;//退货描述
+	private Long returnReasonId;// 退货原因
 
+	private String returnDesc;// 退货描述
 
-    public String getSeries() {
-        return series;
-    }
+	public String getSeries() {
+		return series;
+	}
 
-    public void setSeries(String series) {
-        this.series = series;
-    }
+	public void setSeries(String series) {
+		this.series = series;
+	}
 
-    public String getApplyNumId() {
-        return applyNumId;
-    }
+	public String getApplyNumId() {
+		return applyNumId;
+	}
 
-    public void setApplyNumId(String applyNumId) {
-        this.applyNumId = applyNumId;
-    }
+	public void setApplyNumId(String applyNumId) {
+		this.applyNumId = applyNumId;
+	}
 
-    public Long getDivNumId() {
-        return divNumId;
-    }
+	public Long getDivNumId() {
+		return divNumId;
+	}
 
-    public void setDivNumId(Long divNumId) {
-        this.divNumId = divNumId;
-    }
+	public void setDivNumId(Long divNumId) {
+		this.divNumId = divNumId;
+	}
 
-    public String getBarcode() {
-        return barcode;
-    }
+	public String getBarcode() {
+		return barcode;
+	}
 
-    public void setBarcode(String barcode) {
-        this.barcode = barcode;
-    }
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
 
-    public String getItemName() {
-        return itemName;
-    }
+	public String getItemName() {
+		return itemName;
+	}
 
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
-    }
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
 
-    public Double getLockQty() {
-        return lockQty;
-    }
+	public Double getLockQty() {
+		return lockQty;
+	}
 
-    public void setLockQty(Double lockQty) {
-        this.lockQty = lockQty;
-    }
+	public void setLockQty(Double lockQty) {
+		this.lockQty = lockQty;
+	}
 
-    public Double getStockQty() {
-        return stockQty;
-    }
+	public Double getStockQty() {
+		return stockQty;
+	}
+
+	public void setStockQty(Double stockQty) {
+		this.stockQty = stockQty;
+	}
+
+	public Long getProductOriginNumId() {
+		return productOriginNumId;
+	}
+
+	public void setProductOriginNumId(Long productOriginNumId) {
+		this.productOriginNumId = productOriginNumId;
+	}
+
+	public Double getConversionQty() {
+		return conversionQty;
+	}
+
+	public void setConversionQty(Double conversionQty) {
+		this.conversionQty = conversionQty;
+	}
+
+	public Long getConversionUnitNumId() {
+		return conversionUnitNumId;
+	}
+
+	public void setConversionUnitNumId(Long conversionUnitNumId) {
+		this.conversionUnitNumId = conversionUnitNumId;
+	}
+
+	public String getConversionStyleDesc() {
+		return conversionStyleDesc;
+	}
+
+	public void setConversionStyleDesc(String conversionStyleDesc) {
+		this.conversionStyleDesc = conversionStyleDesc;
+	}
+
+	public Double getPackageQty() {
+		return packageQty;
+	}
+
+	public void setPackageQty(Double packageQty) {
+		this.packageQty = packageQty;
+	}
 
-    public void setStockQty(Double stockQty) {
-        this.stockQty = stockQty;
-    }
+	public Double getQty() {
+		return qty;
+	}
 
-    public Long getProductOriginNumId() {
-        return productOriginNumId;
-    }
+	public void setQty(Double qty) {
+		this.qty = qty;
+	}
 
-    public void setProductOriginNumId(Long productOriginNumId) {
-        this.productOriginNumId = productOriginNumId;
-    }
+	public Double getPmtQty() {
+		return pmtQty;
+	}
 
-    public Double getConversionQty() {
-        return conversionQty;
-    }
+	public void setPmtQty(Double pmtQty) {
+		this.pmtQty = pmtQty;
+	}
 
-    public void setConversionQty(Double conversionQty) {
-        this.conversionQty = conversionQty;
-    }
+	public Long getPmtMaterial() {
+		return pmtMaterial;
+	}
 
-    public Long getConversionUnitNumId() {
-        return conversionUnitNumId;
-    }
+	public void setPmtMaterial(Long pmtMaterial) {
+		this.pmtMaterial = pmtMaterial;
+	}
 
-    public void setConversionUnitNumId(Long conversionUnitNumId) {
-        this.conversionUnitNumId = conversionUnitNumId;
-    }
+	public Double getTradePrice() {
+		return tradePrice;
+	}
 
-    public String getConversionStyleDesc() {
-        return conversionStyleDesc;
-    }
+	public void setTradePrice(Double tradePrice) {
+		this.tradePrice = tradePrice;
+	}
 
-    public void setConversionStyleDesc(String conversionStyleDesc) {
-        this.conversionStyleDesc = conversionStyleDesc;
-    }
+	public Double getTradeAmount() {
+		return tradeAmount;
+	}
 
-    public Double getPackageQty() {
-        return packageQty;
-    }
+	public void setTradeAmount(Double tradeAmount) {
+		this.tradeAmount = tradeAmount;
+	}
 
-    public void setPackageQty(Double packageQty) {
-        this.packageQty = packageQty;
-    }
+	public Double getTaxRate() {
+		return taxRate;
+	}
 
-    public Double getQty() {
-        return qty;
-    }
+	public void setTaxRate(Double taxRate) {
+		this.taxRate = taxRate;
+	}
 
-    public void setQty(Double qty) {
-        this.qty = qty;
-    }
+	public Double getTaxAmount() {
+		return taxAmount;
+	}
 
-    public Double getPmtQty() {
-        return pmtQty;
-    }
+	public void setTaxAmount(Double taxAmount) {
+		this.taxAmount = taxAmount;
+	}
 
-    public void setPmtQty(Double pmtQty) {
-        this.pmtQty = pmtQty;
-    }
+	public Double getOrderQty() {
+		return orderQty;
+	}
 
-    public Long getPmtMaterial() {
-        return pmtMaterial;
-    }
+	public void setOrderQty(Double orderQty) {
+		this.orderQty = orderQty;
+	}
 
-    public void setPmtMaterial(Long pmtMaterial) {
-        this.pmtMaterial = pmtMaterial;
-    }
+	public Long getReturnReasonId() {
+		return returnReasonId;
+	}
 
-    public Double getTradePrice() {
-        return tradePrice;
-    }
+	public void setReturnReasonId(Long returnReasonId) {
+		this.returnReasonId = returnReasonId;
+	}
 
-    public void setTradePrice(Double tradePrice) {
-        this.tradePrice = tradePrice;
-    }
+	public String getReturnDesc() {
+		return returnDesc;
+	}
 
-    public Double getTradeAmount() {
-        return tradeAmount;
-    }
+	public void setReturnDesc(String returnDesc) {
+		this.returnDesc = returnDesc;
+	}
 
-    public void setTradeAmount(Double tradeAmount) {
-        this.tradeAmount = tradeAmount;
-    }
+	public Long getItemNumId() {
+		return itemNumId;
+	}
 
-    public Double getTaxRate() {
-        return taxRate;
-    }
+	public void setItemNumId(Long itemNumId) {
+		this.itemNumId = itemNumId;
+	}
 
-    public void setTaxRate(Double taxRate) {
-        this.taxRate = taxRate;
-    }
+	public String getItemid() {
+		return itemid;
+	}
 
-    public Double getTaxAmount() {
-        return taxAmount;
-    }
+	public void setItemid(String itemid) {
+		this.itemid = itemid;
+	}
 
-    public void setTaxAmount(Double taxAmount) {
-        this.taxAmount = taxAmount;
-    }
-
-    public Double getOrderQty() {
-        return orderQty;
-    }
-
-    public void setOrderQty(Double orderQty) {
-        this.orderQty = orderQty;
-    }
-
-    public Long getReturnReasonId() {
-        return returnReasonId;
-    }
-
-    public void setReturnReasonId(Long returnReasonId) {
-        this.returnReasonId = returnReasonId;
-    }
-
-    public String getReturnDesc() {
-        return returnDesc;
-    }
-
-    public void setReturnDesc(String returnDesc) {
-        this.returnDesc = returnDesc;
-    }
-
-    public Long getItemNumId() {
-        return itemNumId;
-    }
-
-    public void setItemNumId(Long itemNumId) {
-        this.itemNumId = itemNumId;
-    }
 }
