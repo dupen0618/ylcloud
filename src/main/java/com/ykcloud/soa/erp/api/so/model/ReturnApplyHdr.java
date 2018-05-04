0cde2d5848f83aedea7d5cdeb1e679bd92f3300d
shirong.chen <shirong.chen@yiako.com>
2018-05-04 15:28:50 +0800

    退货申请单头查询错误

--- src/main/java/com/ykcloud/soa/erp/api/so/model/ReturnApplyHdr.java
+++ src/main/java/com/ykcloud/soa/erp/api/so/model/ReturnApplyHdr.java
@@ -10,87 +10,85 @@ import java.util.Date;
  */
 public class ReturnApplyHdr implements Serializable {
 
-    private static final long serialVersionUID = 7030264033673568907L;
+	private static final long serialVersionUID = 7030264033673568907L;
 
-    private Long subUnitNumId;//门店编号
+	private Long subUnitNumId;// 门店编号
 
-    private Date orderDate;//申请日期
+	private Date orderDate;// 申请日期
 
-    private Long badGoodsSign;//是否好货
+	private Long badGoodsSign;// 是否好货
 
-    private Long typeNumId;//业务类型
+	private Long typeNumId;// 业务类型
 
-    private Long physicalNumId;//出货物理仓库
+	private Long physicalNumId;// 出货物理仓库
 
-    private Long storageNumId;//出货逻辑仓
+	private Long storageNumId;// 出货逻辑仓
 
-    private Long recStorageNumId;//收货逻辑仓
+	private Long recStorageNumId;// 收货逻辑仓
 
-    private Long recPhySicalNumId;//收货物理仓
+	private Long recPhysicalNumId;// 收货物理仓
 
+	public Long getRecStorageNumId() {
+		return recStorageNumId;
+	}
 
+	public void setRecStorageNumId(Long recStorageNumId) {
+		this.recStorageNumId = recStorageNumId;
+	}
 
-    public Long getRecStorageNumId() {
-        return recStorageNumId;
-    }
+	public Long getRecPhysicalNumId() {
+		return recPhysicalNumId;
+	}
 
-    public void setRecStorageNumId(Long recStorageNumId) {
-        this.recStorageNumId = recStorageNumId;
-    }
+	public void setRecPhysicalNumId(Long recPhysicalNumId) {
+		this.recPhysicalNumId = recPhysicalNumId;
+	}
 
-    public Long getRecPhySicalNumId() {
-        return recPhySicalNumId;
-    }
+	public Long getPhysicalNumId() {
+		return physicalNumId;
+	}
 
-    public void setRecPhySicalNumId(Long recPhySicalNumId) {
-        this.recPhySicalNumId = recPhySicalNumId;
-    }
+	public void setPhysicalNumId(Long physicalNumId) {
+		this.physicalNumId = physicalNumId;
+	}
 
-    public Long getPhysicalNumId() {
-        return physicalNumId;
-    }
+	public Long getStorageNumId() {
+		return storageNumId;
+	}
 
-    public void setPhysicalNumId(Long physicalNumId) {
-        this.physicalNumId = physicalNumId;
-    }
+	public void setStorageNumId(Long storageNumId) {
+		this.storageNumId = storageNumId;
+	}
 
-    public Long getStorageNumId() {
-        return storageNumId;
-    }
+	public Long getTypeNumId() {
+		return typeNumId;
+	}
 
-    public void setStorageNumId(Long storageNumId) {
-        this.storageNumId = storageNumId;
-    }
+	public void setTypeNumId(Long typeNumId) {
+		this.typeNumId = typeNumId;
+	}
 
-    public Long getTypeNumId() {
-        return typeNumId;
-    }
+	public Long getSubUnitNumId() {
+		return subUnitNumId;
+	}
 
-    public void setTypeNumId(Long typeNumId) {
-        this.typeNumId = typeNumId;
-    }
+	public void setSubUnitNumId(Long subUnitNumId) {
+		this.subUnitNumId = subUnitNumId;
+	}
 
-    public Long getSubUnitNumId() {
-        return subUnitNumId;
-    }
+	public Date getOrderDate() {
+		return orderDate;
+	}
 
-    public void setSubUnitNumId(Long subUnitNumId) {
-        this.subUnitNumId = subUnitNumId;
-    }
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
 
-    public Date getOrderDate() {
-        return orderDate;
-    }
+	public Long getBadGoodsSign() {
+		return badGoodsSign;
+	}
 
-    public void setOrderDate(Date orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public Long getBadGoodsSign() {
-        return badGoodsSign;
-    }
-
-    public void setBadGoodsSign(Long badGoodsSign) {
-        this.badGoodsSign = badGoodsSign;
-    }
+	public void setBadGoodsSign(Long badGoodsSign) {
+		this.badGoodsSign = badGoodsSign;
+	}
 }
