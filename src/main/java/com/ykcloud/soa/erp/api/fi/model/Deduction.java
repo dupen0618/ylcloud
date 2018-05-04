761dcdc9e9ce5ef46d11535d010c79ae7ab7d5db
aiden.song <aiden.song@yiako.com>
2018-05-04 14:02:29 +0800

    修改model

--- src/main/java/com/ykcloud/soa/erp/api/fi/model/Deduction.java
+++ src/main/java/com/ykcloud/soa/erp/api/fi/model/Deduction.java
@@ -3,6 +3,8 @@ package com.ykcloud.soa.erp.api.fi.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 /**
  * @Description:获取可结算扣款项明细
  * @author Song
@@ -11,176 +13,145 @@ import java.util.Date;
 public class Deduction implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	private String SERIES; // 行号
-	private Long TENANT_NUM_ID; // 租户编号
-	private Long DATA_SIGN; // 0: 正式 1：测试
-	private Date CREATE_DTME; // 创建时间
-	private Date LAST_UPDTME; // 最后更新时间
-	private Long CREATE_USER_ID; // 创建用户
-	private Long LAST_UPDATE_USER_ID;// 更新用户
-	private String BALANCE_NO; // 结算单号 组合唯一索引
-	private Long SUB_UNIT_NUM_ID; // 门店 分库标识
-	private Date BALANCE_DATE; // 结算日期 分表标识
-	private String RESERVED_NO; // 单据编号 fi_bl_sup_cut_hdr
-	private Long TYPE_NUM_ID; // 业务类型 fi_bl_sup_cut_hdr
-	private Date OPERAT_DATE; // 业务日期
-	private Long CUT_ITEM; // 扣款项目
-	private String CUT_DESC; // 扣款说明
-	private Double CUT_AMOUNT; // 扣款金额
-	private Double TAX_RATE; // 税率
-	private Double TAX_AMOUNT; // 税金
-	private Long CARRY_SIGN; // 结转标识
-
-	public String getSERIES() {
-		return SERIES;
+	private String series; // 行号
+	private Long tenantNumId; // 租户编号
+	private Long dataSign; // 0: 正式 1：测试
+	private Date createDtme; // 创建时间
+	private Date lastUpdtme; // 最后更新时间
+	private Long createUserId; // 创建用户
+	private Long lastUpdateUserId;// 更新用户
+	private String balanceNo; // 结算单号 组合唯一索引
+	private Long subUnitNumId; // 门店 分库标识
+	
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	private Date balanceDate; // 结算日期 分表标识
+	
+	private String reservedNo; // 单据编号 fi_bl_sup_cut_hdr
+	private Long typeNumId; // 业务类型 fi_bl_sup_cut_hdr
+	
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	private Date operatDate; // 业务日期
+	
+	private Long cutItem; // 扣款项目
+	private String cutDesc; // 扣款说明
+	private Double cutAmount; // 扣款金额
+	private Double taxRate; // 税率
+	private Double taxAmount; // 税金
+	private Long carrySign; // 结转标识
+	public String getSeries() {
+		return series;
+	}
+	public void setSeries(String series) {
+		this.series = series;
+	}
+	public Long getTenantNumId() {
+		return tenantNumId;
+	}
+	public void setTenantNumId(Long tenantNumId) {
+		this.tenantNumId = tenantNumId;
+	}
+	public Long getDataSign() {
+		return dataSign;
+	}
+	public void setDataSign(Long dataSign) {
+		this.dataSign = dataSign;
+	}
+	public Date getCreateDtme() {
+		return createDtme;
+	}
+	public void setCreateDtme(Date createDtme) {
+		this.createDtme = createDtme;
+	}
+	public Date getLastUpdtme() {
+		return lastUpdtme;
+	}
+	public void setLastUpdtme(Date lastUpdtme) {
+		this.lastUpdtme = lastUpdtme;
+	}
+	public Long getCreateUserId() {
+		return createUserId;
+	}
+	public void setCreateUserId(Long createUserId) {
+		this.createUserId = createUserId;
+	}
+	public Long getLastUpdateUserId() {
+		return lastUpdateUserId;
+	}
+	public void setLastUpdateUserId(Long lastUpdateUserId) {
+		this.lastUpdateUserId = lastUpdateUserId;
+	}
+	public String getBalanceNo() {
+		return balanceNo;
+	}
+	public void setBalanceNo(String balanceNo) {
+		this.balanceNo = balanceNo;
+	}
+	public Long getSubUnitNumId() {
+		return subUnitNumId;
+	}
+	public void setSubUnitNumId(Long subUnitNumId) {
+		this.subUnitNumId = subUnitNumId;
+	}
+	public Date getBalanceDate() {
+		return balanceDate;
+	}
+	public void setBalanceDate(Date balanceDate) {
+		this.balanceDate = balanceDate;
+	}
+	public String getReservedNo() {
+		return reservedNo;
+	}
+	public void setReservedNo(String reservedNo) {
+		this.reservedNo = reservedNo;
+	}
+	public Long getTypeNumId() {
+		return typeNumId;
+	}
+	public void setTypeNumId(Long typeNumId) {
+		this.typeNumId = typeNumId;
+	}
+	public Date getOperatDate() {
+		return operatDate;
+	}
+	public void setOperatDate(Date operatDate) {
+		this.operatDate = operatDate;
+	}
+	public Long getCutItem() {
+		return cutItem;
+	}
+	public void setCutItem(Long cutItem) {
+		this.cutItem = cutItem;
+	}
+	public String getCutDesc() {
+		return cutDesc;
+	}
+	public void setCutDesc(String cutDesc) {
+		this.cutDesc = cutDesc;
+	}
+	public Double getCutAmount() {
+		return cutAmount;
+	}
+	public void setCutAmount(Double cutAmount) {
+		this.cutAmount = cutAmount;
+	}
+	public Double getTaxRate() {
+		return taxRate;
+	}
+	public void setTaxRate(Double taxRate) {
+		this.taxRate = taxRate;
+	}
+	public Double getTaxAmount() {
+		return taxAmount;
+	}
+	public void setTaxAmount(Double taxAmount) {
+		this.taxAmount = taxAmount;
+	}
+	public Long getCarrySign() {
+		return carrySign;
+	}
+	public void setCarrySign(Long carrySign) {
+		this.carrySign = carrySign;
 	}
 
-	public void setSERIES(String SERIES) {
-		this.SERIES = SERIES;
-	}
-
-	public Long getTENANT_NUM_ID() {
-		return TENANT_NUM_ID;
-	}
-
-	public void setTENANT_NUM_ID(Long TENANT_NUM_ID) {
-		this.TENANT_NUM_ID = TENANT_NUM_ID;
-	}
-
-	public Long getDATA_SIGN() {
-		return DATA_SIGN;
-	}
-
-	public void setDATA_SIGN(Long DATA_SIGN) {
-		this.DATA_SIGN = DATA_SIGN;
-	}
-
-	public Date getCREATE_DTME() {
-		return CREATE_DTME;
-	}
-
-	public void setCREATE_DTME(Date CREATE_DTME) {
-		this.CREATE_DTME = CREATE_DTME;
-	}
-
-	public Date getLAST_UPDTME() {
-		return LAST_UPDTME;
-	}
-
-	public void setLAST_UPDTME(Date LAST_UPDTME) {
-		this.LAST_UPDTME = LAST_UPDTME;
-	}
-
-	public Long getCREATE_USER_ID() {
-		return CREATE_USER_ID;
-	}
-
-	public Long getLAST_UPDATE_USER_ID() {
-		return LAST_UPDATE_USER_ID;
-	}
-
-	public void setLAST_UPDATE_USER_ID(Long lAST_UPDATE_USER_ID) {
-		LAST_UPDATE_USER_ID = lAST_UPDATE_USER_ID;
-	}
-
-	public String getBALANCE_NO() {
-		return BALANCE_NO;
-	}
-
-	public void setBALANCE_NO(String bALANCE_NO) {
-		BALANCE_NO = bALANCE_NO;
-	}
-
-	public Long getSUB_UNIT_NUM_ID() {
-		return SUB_UNIT_NUM_ID;
-	}
-
-	public void setSUB_UNIT_NUM_ID(Long sUB_UNIT_NUM_ID) {
-		SUB_UNIT_NUM_ID = sUB_UNIT_NUM_ID;
-	}
-
-	public Date getBALANCE_DATE() {
-		return BALANCE_DATE;
-	}
-
-	public void setBALANCE_DATE(Date bALANCE_DATE) {
-		BALANCE_DATE = bALANCE_DATE;
-	}
-
-	public String getRESERVED_NO() {
-		return RESERVED_NO;
-	}
-
-	public void setRESERVED_NO(String rESERVED_NO) {
-		RESERVED_NO = rESERVED_NO;
-	}
-
-	public Long getTYPE_NUM_ID() {
-		return TYPE_NUM_ID;
-	}
-
-	public void setTYPE_NUM_ID(Long tYPE_NUM_ID) {
-		TYPE_NUM_ID = tYPE_NUM_ID;
-	}
-
-	public Date getOPERAT_DATE() {
-		return OPERAT_DATE;
-	}
-
-	public void setOPERAT_DATE(Date oPERAT_DATE) {
-		OPERAT_DATE = oPERAT_DATE;
-	}
-
-	public Long getCUT_ITEM() {
-		return CUT_ITEM;
-	}
-
-	public void setCUT_ITEM(Long cUT_ITEM) {
-		CUT_ITEM = cUT_ITEM;
-	}
-
-	public String getCUT_DESC() {
-		return CUT_DESC;
-	}
-
-	public void setCUT_DESC(String cUT_DESC) {
-		CUT_DESC = cUT_DESC;
-	}
-
-	public Double getCUT_AMOUNT() {
-		return CUT_AMOUNT;
-	}
-
-	public void setCUT_AMOUNT(Double cUT_AMOUNT) {
-		CUT_AMOUNT = cUT_AMOUNT;
-	}
-
-	public Double getTAX_RATE() {
-		return TAX_RATE;
-	}
-
-	public void setTAX_RATE(Double tAX_RATE) {
-		TAX_RATE = tAX_RATE;
-	}
-
-	public Double getTAX_AMOUNT() {
-		return TAX_AMOUNT;
-	}
-
-	public void setTAX_AMOUNT(Double tAX_AMOUNT) {
-		TAX_AMOUNT = tAX_AMOUNT;
-	}
-
-	public Long getCARRY_SIGN() {
-		return CARRY_SIGN;
-	}
-
-	public void setCARRY_SIGN(Long cARRY_SIGN) {
-		CARRY_SIGN = cARRY_SIGN;
-	}
-
-	public void setCREATE_USER_ID(Long cREATE_USER_ID) {
-		CREATE_USER_ID = cREATE_USER_ID;
-	}
-
+	
 }
