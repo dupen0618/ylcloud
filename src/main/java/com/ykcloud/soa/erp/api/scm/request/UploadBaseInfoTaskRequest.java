b7339a1ba995f4d91849e54805071d67ddc0b307
Shen, Gaoyun <gaoyun.shen@yiako.com>
2018-05-07 22:28:41 +0800

    证照系统修改

--- src/main/java/com/ykcloud/soa/erp/api/scm/request/UploadBaseInfoTaskRequest.java
+++ src/main/java/com/ykcloud/soa/erp/api/scm/request/UploadBaseInfoTaskRequest.java
@@ -1,6 +1,9 @@
 package com.ykcloud.soa.erp.api.scm.request;
 
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gb.soa.omp.ccommon.api.annotation.ApiField;
 import com.gb.soa.omp.ccommon.api.request.AbstractUserSessionRequest;
 /**
@@ -14,11 +17,12 @@ import com.gb.soa.omp.ccommon.api.request.AbstractUserSessionRequest;
 public class UploadBaseInfoTaskRequest extends AbstractUserSessionRequest {
 	private static final long serialVersionUID = -3936899766322939348L;
 	@ApiField(description = "定时器起始时间，不传就是默认当前时间前一天 yyyy-MM-dd")
-	private String startDate;
-	public String getStartDate() {
+	@JsonFormat
+	private Date startDate;
+	public Date getStartDate() {
 		return startDate;
 	}
-	public void setStartDate(String startDate) {
+	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
 
