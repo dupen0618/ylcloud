3556875e55a6b0de0cf2e20336f4ec17e5986503
james <james.xu@yiako.com>
2018-05-10 18:12:35 +0800

    erp-api的更新

--- src/main/java/com/ykcloud/soa/erp/api/scm/model/AuditReplenishApply.java
+++ src/main/java/com/ykcloud/soa/erp/api/scm/model/AuditReplenishApply.java
@@ -3,15 +3,22 @@ package com.ykcloud.soa.erp.api.scm.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class AuditReplenishApply implements Serializable {
 	
 	/**
 	 * 批量审核的类
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	@NotNull(message="日期不能为空")
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
 	private Date orderDate; //日期
 	
+	@NotNull(message="申请单号不能为空")
 	private String applyNumId;  //申请单单号
 
 	public Date getOrderDate() {
