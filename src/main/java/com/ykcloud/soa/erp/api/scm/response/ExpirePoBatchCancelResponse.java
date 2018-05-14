a1fd5bd637b9733b28a12889f11cab93d57d8f89
xutengzhou <peter.xu@yiako.com>
2018-05-14 08:58:59 +0800

    提交批量作废采购单接口

---
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/ExpirePoBatchCancelResponse.java
@@ -0,0 +1,14 @@
+package com.ykcloud.soa.erp.api.scm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * 采购超时批量作废
+ * @author tz.x
+ * @date 2018年5月12日上午11:12:07
+ */
+public class ExpirePoBatchCancelResponse extends MessagePack {
+
+	private static final long serialVersionUID = -5197506716956425035L;
+
+}
