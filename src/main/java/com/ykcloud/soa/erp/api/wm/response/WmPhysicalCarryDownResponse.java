8315678e421566657b95fd456b4413059ba5db52
Shenq <qiang.shen@yiako.com>
2018-05-14 09:16:15 +0800

    物理仓库月进销存结转

---
+++ src/main/java/com/ykcloud/soa/erp/api/wm/response/WmPhysicalCarryDownResponse.java
@@ -0,0 +1,13 @@
+package com.ykcloud.soa.erp.api.wm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * @author:shenq
+ * @description:物理仓库结转Response
+ * @date :2018/5/11 - 20:31
+ * @version:v1.0
+ */
+public class WmPhysicalCarryDownResponse extends MessagePack {
+
+}
--- src/main/java/com/ykcloud/soa/erp/api/wm/service/WmPhysicalDailyAccountGaService.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/service/WmPhysicalDailyAccountGaService.java
@@ -1,10 +1,20 @@
 package com.ykcloud.soa.erp.api.wm.service;
 
+import com.ykcloud.soa.erp.api.wm.request.WmPhysicalCarryDownRequest;
 import com.ykcloud.soa.erp.api.wm.response.EffectRowsResponse;
+import com.ykcloud.soa.erp.api.wm.response.WmPhysicalCarryDownResponse;
 import com.ykcloud.soa.erp.api.wm.response.WmPhysicalDailyAccountGaInterRequest;
 
 public interface WmPhysicalDailyAccountGaService {
 
     EffectRowsResponse interWmPhysicalDailyAccountGa(WmPhysicalDailyAccountGaInterRequest request);
+
+    /**
+     * 物理仓库月进销存结转
+     * @param request
+     * @return
+     */
+     WmPhysicalCarryDownResponse carryDwonWmPhysical(WmPhysicalCarryDownRequest request);
+
 
 }
