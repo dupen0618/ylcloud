7eaf853cf77b4b85f326d689494604cfe8a724cf
zhaokang <fred.zhao@yiako.com>
2018-05-14 15:35:15 +0800

    批次进销存调整

--- src/main/java/com/ykcloud/soa/erp/api/fi/request/VirtualDailyAndMonthCostGenerateRequest.java
+++ src/main/java/com/ykcloud/soa/erp/api/fi/request/VirtualDailyAndMonthCostGenerateRequest.java
@@ -1,6 +1,7 @@
 package com.ykcloud.soa.erp.api.fi.request;
 
 import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
+import com.gb.soa.omp.ccommon.api.request.AbstractUserSessionRequest;
 import com.ykcloud.soa.erp.api.fi.model.VirtualWmBlBatchDtl;
 
 import javax.validation.constraints.NotNull;
@@ -10,7 +11,7 @@ import javax.validation.constraints.NotNull;
  * @Description:
  * @Date: Created in 20:07:2018/5/11
  */
-public class VirtualDailyAndMonthCostGenerateRequest extends AbstractRequest {
+public class VirtualDailyAndMonthCostGenerateRequest extends AbstractUserSessionRequest {
     private static final long serialVersionUID = -6820833950056653435L;
 
     @NotNull(message = "虚拟批次数据不能为空！")
