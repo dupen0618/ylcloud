60294cf7ec00e53a4ef4904afd49ea0b45e777dd
xutengzhou <peter.xu@yiako.com>
2018-05-15 20:24:21 +0800

    提交库存金额调整接口

--- src/main/java/com/ykcloud/soa/erp/api/wm/service/WmCostAdjustService.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/service/WmCostAdjustService.java
@@ -1,5 +1,20 @@
 package com.ykcloud.soa.erp.api.wm.service;
 
-public interface WmCostAdjustService
-{
+import com.ykcloud.soa.erp.api.wm.request.WmCostByAdjustPriceAdjustRequest;
+import com.ykcloud.soa.erp.api.wm.response.WmCostByAdjustPriceAdjustResponse;
+
+/**
+ * 库存金额调整
+ * @author tz.x
+ * @date 2018年5月15日下午1:27:48
+ */
+public interface WmCostAdjustService {
+	
+	/**
+	 * 调价(不指定批次，不管批次明细中现有价格)【全部调整成新进价】
+	 * @author tz.x
+	 * @date 2018年5月15日下午1:49:01
+	 */
+	public WmCostByAdjustPriceAdjustResponse adjustWmCostByAdjustPrice(WmCostByAdjustPriceAdjustRequest request);
+	
 }
