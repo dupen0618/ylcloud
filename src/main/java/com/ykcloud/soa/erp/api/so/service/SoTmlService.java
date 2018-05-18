ef25f3036119387b0f6857f3c3963165b82a0c13
zhaokang <fred.zhao@yiako.com>
2018-05-18 10:41:17 +0800

    生成销售日报修改

--- src/main/java/com/ykcloud/soa/erp/api/so/service/SoTmlService.java
+++ src/main/java/com/ykcloud/soa/erp/api/so/service/SoTmlService.java
@@ -1,11 +1,9 @@
 package com.ykcloud.soa.erp.api.so.service;
-
 import com.ykcloud.soa.erp.api.so.request.ItemSellDailyDtlNotBalanceCountGetRequest;
 import com.ykcloud.soa.erp.api.so.request.ItemSellDailyDtlSeriesForRegenerateItemSellDailyGetRequest;
 import com.ykcloud.soa.erp.api.so.request.ItemSellDailyDtlStatusNumIdUpdateRequest;
 import com.ykcloud.soa.erp.api.so.request.ItemSellDailyHdrStatusNumIdUpdateRequest;
 import com.ykcloud.soa.erp.api.so.request.ItemSellDailyListForGenerateBalanceGetRequest;
-import com.ykcloud.soa.erp.api.so.request.SdBlSoTmlItemSellDailyRequest;
 import com.ykcloud.soa.erp.api.so.request.SupBlanceDtlByItemSellDailyDtlSeriesGetRequest;
 import com.ykcloud.soa.erp.api.so.request.SupplyUnitNumIdForJointSaleGetRequest;
 import com.ykcloud.soa.erp.api.so.response.ItemSellDailyDtlNotBalanceCountGetResponse;
@@ -13,9 +11,10 @@ import com.ykcloud.soa.erp.api.so.response.ItemSellDailyDtlSeriesForRegenerateIt
 import com.ykcloud.soa.erp.api.so.response.ItemSellDailyDtlStatusNumIdUpdateResponse;
 import com.ykcloud.soa.erp.api.so.response.ItemSellDailyHdrStatusNumIdUpdateResponse;
 import com.ykcloud.soa.erp.api.so.response.ItemSellDailyListForGenerateBalanceGetResponse;
-import com.ykcloud.soa.erp.api.so.response.SdBlSoTmlItemSellDailyResponse;
 import com.ykcloud.soa.erp.api.so.response.SupBlanceDtlByItemSellDailyDtlSeriesGetResponse;
 import com.ykcloud.soa.erp.api.so.response.SupplyUnitNumIdForJointSaleGetResponse;
+import com.ykcloud.soa.erp.api.so.request.SalesDailyGenerateRequest;
+import com.ykcloud.soa.erp.api.so.response.SalesDailyGenerateResponse;
 
 import java.util.List;
 
@@ -25,7 +24,7 @@ import java.util.List;
  * @date :2018/4/23 - 10:01
  * @version:v1.0
  */
-public interface SdBlSoTmlItemSellDailyService {
+public interface SoTmlService {
 
 
 
@@ -35,24 +34,23 @@ public interface SdBlSoTmlItemSellDailyService {
      * @param request
      * @return
      */
-    public SdBlSoTmlItemSellDailyResponse generateSalesDaily(SdBlSoTmlItemSellDailyRequest request);
+    public SalesDailyGenerateResponse generateSalesDaily(SalesDailyGenerateRequest request);
 
 
-
-
-    /**
-     * 根据销售门店编号&&销售日期获取销售日报
-     * @author shenq
-     * @param request
-     * @return
-     */
-    public SdBlSoTmlItemSellDailyResponse getItemSellDaily(SdBlSoTmlItemSellDailyRequest request);
     public ItemSellDailyListForGenerateBalanceGetResponse getItemSellDailyListForGenerateBalance(ItemSellDailyListForGenerateBalanceGetRequest request);
 
     public ItemSellDailyDtlStatusNumIdUpdateResponse updateItemSellDailyDtlStatusNumId(ItemSellDailyDtlStatusNumIdUpdateRequest request);
+    
     public ItemSellDailyHdrStatusNumIdUpdateResponse updateItemSellDailyHdrStatusNumId(ItemSellDailyHdrStatusNumIdUpdateRequest request);
+    
     public SupBlanceDtlByItemSellDailyDtlSeriesGetResponse getSupBlanceDtlByItemSellDailyDtlSeries(SupBlanceDtlByItemSellDailyDtlSeriesGetRequest request);
+    
     public ItemSellDailyDtlSeriesForRegenerateItemSellDailyGetResponse getItemSellDailyDtlSeriesForRegenerateItemSellDaily(ItemSellDailyDtlSeriesForRegenerateItemSellDailyGetRequest request);
+    
     public ItemSellDailyDtlNotBalanceCountGetResponse getItemSellDailyDtlNotBalanceCount(ItemSellDailyDtlNotBalanceCountGetRequest request);
+    
     public SupplyUnitNumIdForJointSaleGetResponse getSupplyUnitNumIdForJointSale(SupplyUnitNumIdForJointSaleGetRequest request);
+    
+    public SalesDailyGenerateResponse getItemSellDaily(SalesDailyGenerateRequest request);
+
 }
