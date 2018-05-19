8198a7c84976dd91ee33f70355ef6a19fc988528
yj <yj.yin@yiako.com>
2018-05-19 13:15:29 +0800

    genxin

--- src/main/java/com/ykcloud/soa/erp/api/fi/response/FiBatchDailyAccountGaResponse.java
+++ src/main/java/com/ykcloud/soa/erp/api/fi/response/FiBatchDailyAccountGaResponse.java
@@ -3,7 +3,11 @@
  */
 package com.ykcloud.soa.erp.api.fi.response;
 
+import java.util.List;
+
 import com.gb.soa.omp.ccommon.api.response.MessagePack;
+import com.ykcloud.soa.erp.api.fi.model.AdjustPriceForApi;
+import com.ykcloud.soa.erp.api.fi.model.BalanceCut;
 
 /**
  * @author 殷剑
@@ -15,5 +19,15 @@ public class FiBatchDailyAccountGaResponse extends MessagePack {
 	 * 
 	 */
 	private static final long serialVersionUID = 4116632841845854107L;
+	
+	private List<AdjustPriceForApi> list;
 
+	public List<AdjustPriceForApi> getList() {
+		return list;
+	}
+
+	public void setList(List<AdjustPriceForApi> list) {
+		this.list = list;
+	}
+	
 }
--- src/main/java/com/ykcloud/soa/erp/api/fi/response/FiBatchMonthAccountGaResponse.java
+++ src/main/java/com/ykcloud/soa/erp/api/fi/response/FiBatchMonthAccountGaResponse.java
@@ -3,7 +3,10 @@
  */
 package com.ykcloud.soa.erp.api.fi.response;
 
+import java.util.List;
+
 import com.gb.soa.omp.ccommon.api.response.MessagePack;
+import com.ykcloud.soa.erp.api.fi.model.AdjustPriceForApi;
 
 /**
  * @author 殷剑
@@ -12,5 +15,15 @@ import com.gb.soa.omp.ccommon.api.response.MessagePack;
 public class FiBatchMonthAccountGaResponse extends MessagePack {
 
 	private static final long serialVersionUID = 8626232377619754200L;
+	
+	private List<AdjustPriceForApi> list;
+
+	public List<AdjustPriceForApi> getList() {
+		return list;
+	}
+
+	public void setList(List<AdjustPriceForApi> list) {
+		this.list = list;
+	}
 
 }
