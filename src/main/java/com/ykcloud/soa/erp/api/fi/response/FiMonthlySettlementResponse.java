46fdf47716ed68e633c2f18115c940094759313c
damon.he@yiako.com <damon.he@yiako.com>
2018-05-22 15:23:02 +0800

    批次月结算，新增文件

---
+++ src/main/java/com/ykcloud/soa/erp/api/fi/response/FiMonthlySettlementResponse.java
@@ -0,0 +1,12 @@
+package com.ykcloud.soa.erp.api.fi.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * @Author Hewei
+ * @Date 2018/5/22 13:56
+ */
+public class FiMonthlySettlementResponse extends MessagePack {
+
+    private static final long serialVersionUID = 8631982369516733603L;
+}
