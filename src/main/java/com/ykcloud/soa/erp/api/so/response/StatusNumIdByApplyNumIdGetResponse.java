d7480367b76397c83bfea13d1735766b2056634e
fakir.jiang <fakir.jiang@yiako.com>
2018-05-22 16:59:46 +0800

    添加状态

---
+++ src/main/java/com/ykcloud/soa/erp/api/so/response/StatusNumIdByApplyNumIdGetResponse.java
@@ -0,0 +1,18 @@
+package com.ykcloud.soa.erp.api.so.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+public class StatusNumIdByApplyNumIdGetResponse extends MessagePack {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long statusNumId;
+
+    public Long getStatusNumId() {
+        return statusNumId;
+    }
+
+    public void setStatusNumId(Long statusNumId) {
+        this.statusNumId = statusNumId;
+    }
+}
---
+++ src/main/java/com/ykcloud/soa/erp/api/so/response/StatusNumIdByApplyNumIdUpdateResponse.java
@@ -0,0 +1,8 @@
+package com.ykcloud.soa.erp.api.so.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+public class StatusNumIdByApplyNumIdUpdateResponse extends MessagePack {
+    private static final long serialVersionUID = 1L;
+
+}
