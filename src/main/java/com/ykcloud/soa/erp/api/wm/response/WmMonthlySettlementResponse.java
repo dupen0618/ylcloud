2c6e4836828e1a90d9aa5d23265222a8a5868909
damon.he@yiako.com <damon.he@yiako.com>
2018-05-22 13:22:33 +0800

    物理仓月结处理 规范异常处理

---
+++ src/main/java/com/ykcloud/soa/erp/api/wm/response/WmMonthlySettlementResponse.java
@@ -0,0 +1,11 @@
+package com.ykcloud.soa.erp.api.wm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * @Author Hewei
+ * @Date 2018/5/22 9:34
+ */
+public class WmMonthlySettlementResponse extends MessagePack {
+    private static final long serialVersionUID = 1265128468841586553L;
+}
