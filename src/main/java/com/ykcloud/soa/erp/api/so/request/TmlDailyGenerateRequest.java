73189a6c2da8d41e7beb7b8e32982d7e6ded2a57
james <james.xu@yiako.com>
2018-05-23 09:56:37 +0800

    erp-api的更新

---
+++ src/main/java/com/ykcloud/soa/erp/api/so/request/TmlDailyGenerateRequest.java
@@ -0,0 +1,42 @@
+package com.ykcloud.soa.erp.api.so.request;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
+
+public class TmlDailyGenerateRequest extends AbstractRequest {
+
+	/**
+	 * 小票
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@NotNull(message="门店不能为空")
+	private Long subUnitNumId;   //门店
+	
+	@NotNull(message="日期不能为空")
+	@JsonFormat(pattern = "yyyy-MM-dd ", timezone = "GMT+8")
+	private Date orderDate;
+
+	public Long getSubUnitNumId() {
+		return subUnitNumId;
+	}
+
+	public void setSubUnitNumId(Long subUnitNumId) {
+		this.subUnitNumId = subUnitNumId;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	
+	
+
+}
