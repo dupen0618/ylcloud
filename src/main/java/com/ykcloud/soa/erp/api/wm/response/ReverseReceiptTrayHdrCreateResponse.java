17e7b536281272d0b501659b2b3099fd71ec7159
Shen, Gaoyun <gaoyun.shen@yiako.com>
2018-05-29 21:01:02 +0800

    代码修改

--- src/main/java/com/ykcloud/soa/erp/api/wm/response/ReverseReceiptTrayHdrCreateResponse.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/response/ReverseReceiptTrayHdrCreateResponse.java
@@ -10,17 +10,18 @@ import com.gb.soa.omp.ccommon.api.response.MessagePack;
 public class ReverseReceiptTrayHdrCreateResponse extends MessagePack {
 	private static final long serialVersionUID = -3472757841303125392L;
 	private String traySerlno;//托盘号
-	private String traySerlnoLable;//托盘实物标签
+	private String traySerlnoLabel;//托盘实物标签
 	public String getTraySerlno() {
 		return traySerlno;
 	}
 	public void setTraySerlno(String traySerlno) {
 		this.traySerlno = traySerlno;
 	}
-	public String getTraySerlnoLable() {
-		return traySerlnoLable;
+	public String getTraySerlnoLabel() {
+		return traySerlnoLabel;
 	}
-	public void setTraySerlnoLable(String traySerlnoLable) {
-		this.traySerlnoLable = traySerlnoLable;
+	public void setTraySerlnoLabel(String traySerlnoLabel) {
+		this.traySerlnoLabel = traySerlnoLabel;
 	}
+	
 }
