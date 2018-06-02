b9207743fe22f87d8cbf4fe84d39868d08c99d91
alfred <liu@yiako.com>
2018-06-02 18:05:54 +0800

    购销

---
+++ src/main/java/com/ykcloud/soa/erp/api/fi/response/CustPayAuditResponse.java
@@ -0,0 +1,7 @@
+package com.ykcloud.soa.erp.api.fi.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+public class CustPayAuditResponse extends MessagePack {
+    private static final long serialVersionUID = 9174383332570667141L;
+}
