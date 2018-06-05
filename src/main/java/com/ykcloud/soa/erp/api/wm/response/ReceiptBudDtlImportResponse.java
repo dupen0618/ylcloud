fb32813a04763fbe018460088c54a6e279d09418
yj <yj.yin@yiako.com>
2018-06-05 17:59:43 +0800

    殷剑 Excel批量导入 2018/6/5 18:00

--- src/main/java/com/ykcloud/soa/erp/api/wm/response/ReceiptBudDtlImportResponse.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/response/ReceiptBudDtlImportResponse.java
@@ -3,7 +3,10 @@
  */
 package com.ykcloud.soa.erp.api.wm.response;
 
+import java.util.List;
+
 import com.gb.soa.omp.ccommon.api.response.MessagePack;
+import com.ykcloud.soa.erp.api.scm.model.ImportError;
 
 /**
  * @author 殷剑
@@ -19,6 +22,11 @@ public class ReceiptBudDtlImportResponse extends MessagePack {
 	
 	//验收单
 	private String reservedNo;
+	
+	/**
+	 * 导入错误明细
+	 */
+	private List<ImportError> importErrors;
 
 	public String getReservedNo() {
 		return reservedNo;
@@ -27,9 +35,13 @@ public class ReceiptBudDtlImportResponse extends MessagePack {
 	public void setReservedNo(String reservedNo) {
 		this.reservedNo = reservedNo;
 	}
-	
-	
 
+	public List<ImportError> getImportErrors() {
+		return importErrors;
+	}
 
-	
+	public void setImportErrors(List<ImportError> importErrors) {
+		this.importErrors = importErrors;
+	}
+
 }
