ef7c4b3a6429e0b2b097f3c6071286be550c081d
Shen, Gaoyun <gaoyun.shen@yiako.com>
2018-06-05 10:28:56 +0800

    托盘明细添加实体

--- src/main/java/com/ykcloud/soa/erp/api/wm/response/ReceiptTrayDetailListGetResponse.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/response/ReceiptTrayDetailListGetResponse.java
@@ -1,8 +1,22 @@
 package com.ykcloud.soa.erp.api.wm.response;
 
+import java.util.List;
+
 import com.gb.soa.omp.ccommon.api.response.MessagePack;
+import com.ykcloud.soa.erp.api.wm.model.ReceiptTrayInfo;
 
 public class ReceiptTrayDetailListGetResponse extends MessagePack {
 
     private static final long serialVersionUID = -8716207623324225827L;
+    
+    private List<ReceiptTrayInfo> list;
+
+	public List<ReceiptTrayInfo> getList() {
+		return list;
+	}
+
+	public void setList(List<ReceiptTrayInfo> list) {
+		this.list = list;
+	}
+    
 }
