2e30c02b28e65523651957e2685f803d1b27948e
xutengzhou <peter.xu@yiako.com>
2018-06-11 19:25:20 +0800

    包装获取销售日期接口

--- src/main/java/com/ykcloud/soa/erp/api/wm/service/WmQueryService.java
+++ src/main/java/com/ykcloud/soa/erp/api/wm/service/WmQueryService.java
@@ -1,8 +1,10 @@
 package com.ykcloud.soa.erp.api.wm.service;
 
 import com.ykcloud.soa.erp.api.wm.request.BackOffRateGetRequest;
+import com.ykcloud.soa.erp.api.wm.request.BillInAccountDateGetRequest;
 import com.ykcloud.soa.erp.api.wm.request.SupPriceBySeriesGetRequest;
 import com.ykcloud.soa.erp.api.wm.response.BackOffRateGetResponse;
+import com.ykcloud.soa.erp.api.wm.response.BillInAccountDateGetResponse;
 import com.ykcloud.soa.erp.api.wm.response.SupPriceBySeriesGetResponse;
 
 /**
@@ -11,6 +13,14 @@ import com.ykcloud.soa.erp.api.wm.response.SupPriceBySeriesGetResponse;
  * @Date: 2018/4/18 16:00
  */
 public interface WmQueryService {
+	
+	/**
+	 * 获取入账日期（包装）
+	 * @author tz.x
+	 * @date 2018年6月11日下午7:18:25
+	 */
+	public BillInAccountDateGetResponse getBillInAccountDate(BillInAccountDateGetRequest request);
+	
     //查询倒扣率
     public BackOffRateGetResponse getBackOffRate(BackOffRateGetRequest request);
 
