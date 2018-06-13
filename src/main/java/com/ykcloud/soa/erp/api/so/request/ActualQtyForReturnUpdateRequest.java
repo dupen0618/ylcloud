3996738344530ebf87f50ce45124bd008bc187bf
xutengzhou <peter.xu@yiako.com>
2018-06-13 13:39:15 +0800

    更新代码

--- src/main/java/com/ykcloud/soa/erp/api/so/request/ActualQtyForReturnUpdateRequest.java
+++ src/main/java/com/ykcloud/soa/erp/api/so/request/ActualQtyForReturnUpdateRequest.java
@@ -3,15 +3,22 @@ package com.ykcloud.soa.erp.api.so.request;
 import java.util.Date;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
 import com.ykcloud.soa.erp.api.scm.model.UpdateActualQty;
 
 public class ActualQtyForReturnUpdateRequest extends AbstractRequest {
 	
+	@NotNull(message = "门店编号不能为空！")
 	private Long subUnitNumId;     //门店编号
 	
+	@NotNull(message = "订单日期不能为空！")
 	private Date orderDate;       //日期
 	
+	@NotEmpty(message = "调整明细不能为空！")
 	private List<UpdateActualQty> updateActualQtyList;
 	
 	public List<UpdateActualQty> getUpdateActualQtyList() {
