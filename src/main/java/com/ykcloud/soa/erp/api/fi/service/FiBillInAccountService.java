a4ee091380296c60d3b8e25684e6829b432b7690
damon.he@yiako.com <damon.he@yiako.com>
2018-06-14 16:05:31 +0800

    增加发出商品、发出商品代销商品款，月结处理

--- src/main/java/com/ykcloud/soa/erp/api/fi/service/FiBillInAccountService.java
+++ src/main/java/com/ykcloud/soa/erp/api/fi/service/FiBillInAccountService.java
@@ -10,9 +10,14 @@ import com.ykcloud.soa.erp.api.fi.response.ReceiptInAccountResponse;
  * @Date 2018/4/10 11:30
  */
 public interface FiBillInAccountService {
+    //通用入账
     ReceiptInAccountResponse setReceiptInAccountService(ReceiptInAccountRequest request);
-
+    //批次月进销，月结
     FiMonthlySettlementResponse buildBatchMonthSalesMonthlySettlementRecord(FiMonthlySettlementRequest request);
-
+    //商品成本月，月结
     FiMonthlySettlementResponse buildItemMonthSalesMonthlySettlementRecord(FiMonthlySettlementRequest request);
+    //发出商品，月结
+    FiMonthlySettlementResponse buildSendProductSalesMonthlySettlementRecord(FiMonthlySettlementRequest request);
+    //发出商品代销商品款，月结
+    FiMonthlySettlementResponse buildSendProductConsignmentSalesMonthlySettlementRecord(FiMonthlySettlementRequest request);
 }
