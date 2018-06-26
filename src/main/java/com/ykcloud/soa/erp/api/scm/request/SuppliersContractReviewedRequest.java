b3a5bb6a7f6c0a1077b3603cfd6acc2e7b52b94b
damon.he@yiako.com <damon.he@yiako.com>
2018-06-26 15:12:11 +0800

    Excel导入：供应商信息、合同 合同移动到SCM系统

---
+++ src/main/java/com/ykcloud/soa/erp/api/scm/request/SuppliersContractReviewedRequest.java
@@ -0,0 +1,22 @@
+package com.ykcloud.soa.erp.api.scm.request;
+
+import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
+
+public class SuppliersContractReviewedRequest extends AbstractRequest {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8054081068925132144L;
+	
+	private Long batchNo;
+
+	public Long getBatchNo() {
+		return batchNo;
+	}
+
+	public void setBatchNo(Long batchNo) {
+		this.batchNo = batchNo;
+	} 
+
+}
