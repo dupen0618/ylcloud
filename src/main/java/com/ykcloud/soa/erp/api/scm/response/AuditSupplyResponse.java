0029707756588ae543962c2e91293f8c98bca545
damon.he@yiako.com <damon.he@yiako.com>
2018-06-26 18:50:38 +0800

    供应商、合同，修改

--- src/main/java/com/ykcloud/soa/erp/api/scm/response/AuditSupplyResponse.java
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/AuditSupplyResponse.java
@@ -2,7 +2,7 @@ package com.ykcloud.soa.erp.api.scm.response;
 
 import com.gb.soa.omp.ccommon.api.response.MessagePack;
 
-public class SuppliersContractReviewedResponse extends MessagePack {
+public class AuditSupplyResponse extends MessagePack {
 
 	/**
 	 * 
