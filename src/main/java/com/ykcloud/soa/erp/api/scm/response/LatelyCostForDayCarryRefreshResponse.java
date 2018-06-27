e943512d7a1493f4697391fbf98a79ea40e441bd
xutengzhou <peter.xu@yiako.com>
2018-06-27 11:09:47 +0800

    提交进价调整审核

---
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/LatelyCostForDayCarryRefreshResponse.java
@@ -0,0 +1,14 @@
+package com.ykcloud.soa.erp.api.scm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * 日结刷新进价
+ * @author tz.x
+ * @date 2018年6月27日上午8:48:12
+ */
+public class LatelyCostForDayCarryRefreshResponse extends MessagePack {
+
+	private static final long serialVersionUID = 4443179564117706489L;
+
+}
