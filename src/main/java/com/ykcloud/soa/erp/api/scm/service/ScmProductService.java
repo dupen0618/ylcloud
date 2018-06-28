f098638167853002eaefd0697ba92cb0c0535bac
xutengzhou <peter.xu@yiako.com>
2018-06-28 16:36:17 +0800

    测试导入门店商品销售属性

--- src/main/java/com/ykcloud/soa/erp/api/scm/service/ScmProductService.java
+++ src/main/java/com/ykcloud/soa/erp/api/scm/service/ScmProductService.java
@@ -3,6 +3,7 @@ package com.ykcloud.soa.erp.api.scm.service;
 import com.ykcloud.soa.erp.api.scm.request.GiftQtyByGiftRelationGetRequest;
 
 import com.ykcloud.soa.erp.api.scm.request.ProductPurchasePriceGetRequest;
+import com.ykcloud.soa.erp.api.scm.request.ProductShopRequest;
 import com.ykcloud.soa.erp.api.scm.request.ProtocolAuditRequest;
 import com.ykcloud.soa.erp.api.scm.response.ProductForReplenishCheckResponse;
 import com.ykcloud.soa.erp.api.scm.response.GiftQtyByGiftRelationGetResponse;
@@ -10,11 +11,22 @@ import com.ykcloud.soa.erp.api.scm.request.ProductForReplenishCheckRequest;
 import com.ykcloud.soa.erp.api.scm.response.ProductFdcSignBySupplyNumIdAndPty3NumIdGetResponse;
 import com.ykcloud.soa.erp.api.scm.request.ProductFdcSignBySupplyNumIdAndPty3NumIdGetRequest;
 import com.ykcloud.soa.erp.api.scm.response.ProductPurchasePriceGetResponse;
+import com.ykcloud.soa.erp.api.scm.response.ProductShopResponse;
 import com.ykcloud.soa.erp.api.scm.response.ProtocolAuditResponse;
 
 
 
 public interface ScmProductService {
+	
+	/**
+	 * 
+	 * @description
+	 * @author gaoyun.shen
+	 * @date: 2018年6月22日 下午4:27:34
+	 * @param 
+	 * @return  门店商品销售属性 excel导入
+	 */
+	public ProductShopResponse importProductShop(ProductShopRequest request);
 
 	//获取供应商品赠品数量
 	public GiftQtyByGiftRelationGetResponse getGiftQtyByGiftRelation(GiftQtyByGiftRelationGetRequest request);
