ba986e0fab5f4213b49192ed961226f092c950b6
pengh <pengh@LAPTOP-KB3GVGKK>
2018-06-28 12:04:39 +0800

    削价审核

---
+++ src/main/java/com/ykcloud/soa/erp/api/scm/response/PriceCatAuditResponse.java
@@ -0,0 +1,14 @@
+package com.ykcloud.soa.erp.api.scm.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * 削价单审核
+ * @author pengh
+ *
+ */
+public class PriceCatAuditResponse extends MessagePack {
+
+	private static final long serialVersionUID = -8081291865251114090L;
+
+}
