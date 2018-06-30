b40fdeb46be5fbb80f27850918532ce4a43fd331
shirong.chen <shirong.chen@yiako.com>
2018-06-30 17:31:49 +0800

    删除废弃的类

--- src/main/java/com/ykcloud/soa/erp/api/scm/service/ScmSupplyService.java
+++ src/main/java/com/ykcloud/soa/erp/api/scm/service/ScmSupplyService.java
@@ -6,9 +6,6 @@ import com.ykcloud.soa.erp.api.scm.response.*;
 
 public interface ScmSupplyService {
   
-	//获得直通门店供货商订货周期
-  	public DirectWayReplenishweeklyDayFlagGetResponse getDirectWayReplenishweeklyDayFlag(DirectWayReplenishweeklyDayFlagGetRequest Request);
-
   	//查询直送门店的供应商订货周期、有效天数
   	public DirectDeliveryReplenishweeklyDayFlagAndEffectiveDayGetResponse  getDirectDeliveryReplenishweeklyDayFlagAndEffectiveDay(
   			DirectDeliveryReplenishweeklyDayFlagAndEffectiveDayGetRequest request);
