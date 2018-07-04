a2c01343a9a78f90c84f4eca8f99a6d5a555e89c
ALi <ali.pan@yiako.com>
2018-07-04 11:41:17 +0800

    批量生成结算单和供应商信息

--- src/main/java/com/ykcloud/soa/erp/api/fi/request/BalanceDataBatchGenerateRequest.java
+++ src/main/java/com/ykcloud/soa/erp/api/fi/request/BalanceDataBatchGenerateRequest.java
@@ -17,54 +17,7 @@ public class BalanceDataBatchGenerateRequest extends AbstractUserSessionRequest 
 
 	private static final long serialVersionUID = 4627685577873375711L;
 
-	@NotNull(message = "门店编号不能为空！")
-	private Long subUnitNumId;
-
-	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-	@NotNull(message = "结算日期不能为空！")
-	private Date balanceDate;
-
-	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-	@NotNull(message = "开始日期不能为空！")
-	private Date startDate;
-
-	@NotNull(message = "结算方式不能为空！")
-	private Long settlementType;
-
 	List<BatchInsertBalance>  batchInsertBalanceList;
-
-	public Long getSubUnitNumId() {
-		return subUnitNumId;
-	}
-
-	public void setSubUnitNumId(Long subUnitNumId) {
-		this.subUnitNumId = subUnitNumId;
-	}
-
-	public Long getSettlementType() {
-		return settlementType;
-	}
-
-	public void setSettlementType(Long settlementType) {
-		this.settlementType = settlementType;
-	}
-
-	public Date getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-
-
-	public Date getBalanceDate() {
-		return balanceDate;
-	}
-
-	public void setBalanceDate(Date balanceDate) {
-		this.balanceDate = balanceDate;
-	}
 
 	public List<BatchInsertBalance> getBatchInsertBalanceList() {
 		return batchInsertBalanceList;
