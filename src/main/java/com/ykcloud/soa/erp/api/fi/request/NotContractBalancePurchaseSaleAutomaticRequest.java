45e51838723fb4d610da5d19d26c8d997cdaf20f
alfred <liu@yiako.com>
2018-07-06 14:06:28 +0800

    购销联销获取供应商自动结算单

---
+++ src/main/java/com/ykcloud/soa/erp/api/fi/request/NotContractBalancePurchaseSaleAutomaticRequest.java
@@ -0,0 +1,32 @@
+package com.ykcloud.soa.erp.api.fi.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.gb.soa.omp.ccommon.api.request.AbstractUserSessionRequest;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+public class NotContractBalancePurchaseSaleAutomaticRequest extends AbstractUserSessionRequest {
+    private static final long serialVersionUID = -1766553622327456524L;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    @NotNull(message = "结算日期不能为空！")
+    private Date balanceDate;
+    @NotNull(message = "门店编号不能为空！")
+    private Long subUnitNumId;
+
+    public Date getBalanceDate() {
+        return balanceDate;
+    }
+
+    public void setBalanceDate(Date balanceDate) {
+        this.balanceDate = balanceDate;
+    }
+
+    public Long getSubUnitNumId() {
+        return subUnitNumId;
+    }
+
+    public void setSubUnitNumId(Long subUnitNumId) {
+        this.subUnitNumId = subUnitNumId;
+    }
+}
