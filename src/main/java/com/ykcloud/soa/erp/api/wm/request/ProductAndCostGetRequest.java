4bdfdade9ffeedac13e9dcf1330dda8beb570817
fakir.jiang <fakir.jiang@yiako.com>
2018-07-10 20:32:56 +0800

    获取商品信息和价格

---
+++ src/main/java/com/ykcloud/soa/erp/api/wm/request/ProductAndCostGetRequest.java
@@ -0,0 +1,32 @@
+package com.ykcloud.soa.erp.api.wm.request;
+
+import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
+
+import javax.validation.constraints.NotNull;
+
+public class ProductAndCostGetRequest extends AbstractRequest {
+
+    private static final long serialVersionUID = 2202636539056219049L;
+
+    @NotNull(message = "门店编号不能为空")
+    private Long subUnitNumId;
+
+    @NotNull(message="商品编号不能为空")
+    private Long itemNumId;
+
+    public Long getSubUnitNumId() {
+        return subUnitNumId;
+    }
+
+    public void setSubUnitNumId(Long subUnitNumId) {
+        this.subUnitNumId = subUnitNumId;
+    }
+
+    public Long getItemNumId() {
+        return itemNumId;
+    }
+
+    public void setItemNumId(Long itemNumId) {
+        this.itemNumId = itemNumId;
+    }
+}
