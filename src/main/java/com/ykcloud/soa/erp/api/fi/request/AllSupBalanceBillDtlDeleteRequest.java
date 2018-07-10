eb90d865480731200a8db95224acac0930cad466
ALi <ali.pan@yiako.com>
2018-07-10 09:39:53 +0800

    结算

---
+++ src/main/java/com/ykcloud/soa/erp/api/fi/request/AllSupBalanceBillDtlDeleteRequest.java
@@ -0,0 +1,48 @@
+package com.ykcloud.soa.erp.api.fi.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.gb.soa.omp.ccommon.api.request.AbstractUserSessionRequest;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * @Description:删除结算单商品
+ * @author ali
+ * @Date 2018年4月4日 下午5:18:14
+ */
+public class AllSupBalanceBillDtlDeleteRequest extends AbstractUserSessionRequest {
+	private static final long serialVersionUID = 5418678300240637435L;
+	@NotNull(message = "门店编号不能为空！")
+	private Long subUnitNumId;
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	@NotNull(message = "结算日期不能为空！")
+	private Date balanceDate;
+	@NotNull(message = "结算单号不能为空！")
+	private String balanceNo;
+
+	public Long getSubUnitNumId() {
+		return subUnitNumId;
+	}
+
+	public void setSubUnitNumId(Long subUnitNumId) {
+		this.subUnitNumId = subUnitNumId;
+	}
+
+	public Date getBalanceDate() {
+		return balanceDate;
+	}
+
+	public void setBalanceDate(Date balanceDate) {
+		this.balanceDate = balanceDate;
+	}
+
+	public String getBalanceNo() {
+		return balanceNo;
+	}
+
+	public void setBalanceNo(String balanceNo) {
+		this.balanceNo = balanceNo;
+	}
+
+}
