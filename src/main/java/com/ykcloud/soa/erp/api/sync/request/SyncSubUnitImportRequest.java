624973cbe88156816f9932ce71580ce9a5b34674
HansYan <hans.yan@yiako.com>
2018-07-10 09:24:01 +0800

    导入 --门店

--- src/main/java/com/ykcloud/soa/erp/api/sync/request/SyncSubUnitImportRequest.java
+++
@@ -1,6 +0,0 @@
-package com.ykcloud.soa.erp.api.sync.request;
-
-import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
-
-public class SyncSubUnitImportRequest extends AbstractRequest {
-}
--- src/main/java/com/ykcloud/soa/erp/api/sync/service/SyncShopService.java
+++ src/main/java/com/ykcloud/soa/erp/api/sync/service/SyncShopService.java
@@ -1,10 +1,9 @@
 package com.ykcloud.soa.erp.api.sync.service;
 
 
-import com.ykcloud.soa.erp.api.sync.request.SyncSubUnitImportRequest;
 import com.ykcloud.soa.erp.api.sync.response.SyncSubUnitImportResponse;
 
 public interface SyncShopService {
 
-	SyncSubUnitImportResponse syncSubUnit(SyncSubUnitImportRequest request);
+	SyncSubUnitImportResponse syncSubUnit();
 }
