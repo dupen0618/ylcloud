fc5400a4ac6fc2467226893c6f3c3a5c89d8b39c
pengh <pengh@LAPTOP-KB3GVGKK>
2018-07-10 20:18:03 +0800

    会员调价同步 临时、永久调价同步

---
+++ src/main/java/com/ykcloud/soa/erp/api/sync/response/ChangePriceSynResponse.java
@@ -0,0 +1,12 @@
+package com.ykcloud.soa.erp.api.sync.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+/**
+ * 临时、永久调价数据同步
+ * @author pengh
+ *
+ */
+public class ChangePriceSynResponse extends MessagePack {
+	private static final long serialVersionUID = 1L;
+	
+}
