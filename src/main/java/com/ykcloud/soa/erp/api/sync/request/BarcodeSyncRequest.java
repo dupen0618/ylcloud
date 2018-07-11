694253f6a54b0948f1a87139d7dd8dafd9fef4c5
dan <dan.wang@yiako.com>
2018-07-11 10:54:09 +0800

    数据同步

---
+++ src/main/java/com/ykcloud/soa/erp/api/sync/request/BarcodeSyncRequest.java
@@ -0,0 +1,12 @@
+package com.ykcloud.soa.erp.api.sync.request;
+
+import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
+
+public class BarcodeSyncRequest extends AbstractRequest {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1384419347813096484L;
+
+}
