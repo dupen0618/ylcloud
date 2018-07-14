15f6cdd9fbc0a496bdeba5c910760f4acfe96b43
Sealin <sealin.li@yiako.com>
2018-07-14 17:44:39 +0800

    规范命名

--- src/main/java/com/ykcloud/soa/erp/api/fi/response/RollbackAuditionResponse.java
+++ src/main/java/com/ykcloud/soa/erp/api/fi/response/RollbackAuditionResponse.java
@@ -8,6 +8,6 @@ import com.gb.soa.omp.ccommon.api.response.MessagePack;
  * @author Sealin
  * @date 2018-07-14
  */
-public class BalanceAuditRollbackResponse extends MessagePack {
+public class RollbackAuditionResponse extends MessagePack {
     private static final long serialVersionUID = -3646067183027033058L;
 }
