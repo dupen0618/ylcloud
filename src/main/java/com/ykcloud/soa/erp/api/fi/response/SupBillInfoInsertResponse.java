6a4d07ae36f7011eac894cc0070cabef87b524ea
lixin <lzxy12923@163.com>
2018-07-16 21:33:50 +0800

    批量新增发票request

---
+++ src/main/java/com/ykcloud/soa/erp/api/fi/response/SupBillInfoInsertResponse.java
@@ -0,0 +1,7 @@
+package com.ykcloud.soa.erp.api.fi.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+public class SupBillInfoInsertResponse  extends MessagePack {
+    private static final long serialVersionUID = 3385729312021344660L;
+}
