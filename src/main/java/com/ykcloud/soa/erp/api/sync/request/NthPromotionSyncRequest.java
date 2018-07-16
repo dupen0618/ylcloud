8425bafeb4dedaca7cbb6214d0a805f1a9dca981
dan <dan.wang@yiako.com>
2018-07-16 17:59:12 +0800

    数据导入

---
+++ src/main/java/com/ykcloud/soa/erp/api/sync/request/NthPromotionSyncRequest.java
@@ -0,0 +1,12 @@
+package com.ykcloud.soa.erp.api.sync.request;
+
+import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
+
+public class NthPromotionSyncRequest extends AbstractRequest {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7980598760188859502L;
+
+}
---
+++ src/main/java/com/ykcloud/soa/erp/api/sync/response/NthPromotionSyncResponse.java
@@ -0,0 +1,12 @@
+package com.ykcloud.soa.erp.api.sync.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+public class NthPromotionSyncResponse extends MessagePack {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1396224576310682451L;
+
+}
