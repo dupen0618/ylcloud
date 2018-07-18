6857384bbe174e73b6bf4565c4f1ee55ee886cc9
pengh <pengh@LAPTOP-KB3GVGKK>
2018-07-18 13:27:07 +0800

    打包促销 组合促销同步

---
+++ src/main/java/com/ykcloud/soa/erp/api/sync/response/CombinePromotionSysResponse.java
@@ -0,0 +1,12 @@
+package com.ykcloud.soa.erp.api.sync.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+public class CombinePromotionSysResponse extends MessagePack {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1820108102203173941L;
+
+}
