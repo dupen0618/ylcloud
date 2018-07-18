30d7f1ab4e87ce0fc345bcafa52625c156003018
chenhongwei <vincent.chen@yiako.com>
2018-07-18 12:49:11 +0800

    全量促销导入

---
+++ src/main/java/com/ykcloud/soa/erp/api/sync/request/AllPromotionSyncRequest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2017-2018 All Rights Reserved
+ * 上海仰空网络科技有限公司 版权所有
+ */
+
+package com.ykcloud.soa.erp.api.sync.request;
+
+import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
+
+/**
+ * @className AllPromotionRequest
+ * @description 同步全量促销
+ */
+public class AllPromotionSyncRequest extends AbstractRequest {
+    private static final long serialVersionUID = 3059857266496001230L;
+}
---
+++ src/main/java/com/ykcloud/soa/erp/api/sync/response/AllPromotionSyncResponse.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2017-2018 All Rights Reserved
+ * 上海仰空网络科技有限公司 版权所有
+ */
+
+package com.ykcloud.soa.erp.api.sync.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+/**
+ * @className AllPromotionResponse
+ * @description 全量促销同步
+ */
+public class AllPromotionSyncResponse extends MessagePack {
+    private static final long serialVersionUID = -7201109306439188355L;
+}
