abed3d1452486b939da084300a7ed4d3100bfa57
chenhongwei <vincent.chen@yiako.com>
2018-07-18 18:20:45 +0800

    时点促销导入

---
+++ src/main/java/com/ykcloud/soa/erp/api/sync/request/TimePointPromotionSyncRequest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2017-2018 All Rights Reserved
+ * 上海仰空网络科技有限公司 版权所有
+ */
+
+package com.ykcloud.soa.erp.api.sync.request;
+
+import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
+
+/**
+ * @className TimePointPromotionSyncRequest
+ * @description
+ */
+public class TimePointPromotionSyncRequest extends AbstractRequest {
+    private static final long serialVersionUID = 4311068952520831308L;
+}
