7c384d701c65a7c386429e122aae3486c105a381
Sealin <sealin.li@yiako.com>
2018-07-19 10:42:32 +0800

    完善批次/日月进销存日志信息

--- src/main/java/com/ykcloud/soa/erp/api/fi/request/CostAmountAndTaxAmountAdjustRequest.java
+++ src/main/java/com/ykcloud/soa/erp/api/fi/request/CostAmountAndTaxAmountAdjustRequest.java
@@ -1,11 +1,10 @@
 package com.ykcloud.soa.erp.api.fi.request;
 
-import java.util.List;
-
-import javax.validation.constraints.NotNull;
-
 import com.gb.soa.omp.ccommon.api.request.AbstractUserSessionRequest;
 import com.ykcloud.soa.erp.api.fi.model.CostAmountAndTaxAmountForAdjust;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * 商品成本金额调整
@@ -13,11 +12,11 @@ import com.ykcloud.soa.erp.api.fi.model.CostAmountAndTaxAmountForAdjust;
  * @date 2018年5月16日下午8:51:03
  */
 public class CostAmountAndTaxAmountAdjustRequest extends AbstractUserSessionRequest {
-
-	private static final long serialVersionUID = 8621368692202250424L;
-	
+	private static final long serialVersionUID = 4002463897456845900L;
 	@NotNull(message = "门店不能为空！")
 	private Long subUnitNumId;
+
+	private Long typeNumId;
 	
 	//@NotEmpty(message = "商品成本金额调整明细不能为空！")
 	private List<CostAmountAndTaxAmountForAdjust> fiCostDetails;
@@ -37,6 +36,12 @@ public class CostAmountAndTaxAmountAdjustRequest extends AbstractUserSessionRequ
 	public void setFiCostDetails(List<CostAmountAndTaxAmountForAdjust> fiCostDetails) {
 		this.fiCostDetails = fiCostDetails;
 	}
-	
 
+	public Long getTypeNumId() {
+		return typeNumId;
+	}
+
+	public void setTypeNumId(Long typeNumId) {
+		this.typeNumId = typeNumId;
+	}
 }
