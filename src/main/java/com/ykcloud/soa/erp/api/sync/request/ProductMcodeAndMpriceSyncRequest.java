52579ba5f0c686397ec060dc5c4e4d2916780aed
dan <dan.wang@yiako.com>
2018-07-20 20:07:29 +0800

    一品多码审核

---
+++ src/main/java/com/ykcloud/soa/erp/api/sync/request/ProductMcodeAndMpriceSyncRequest.java
@@ -0,0 +1,12 @@
+package com.ykcloud.soa.erp.api.sync.request;
+
+import com.gb.soa.omp.ccommon.api.request.AbstractRequest;
+
+public class ProductMcodeAndMpriceSyncRequest extends AbstractRequest {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 579336930917656876L;
+
+}
---
+++ src/main/java/com/ykcloud/soa/erp/api/sync/response/ProductMcodeAndMpriceSyncResponse.java
@@ -0,0 +1,12 @@
+package com.ykcloud.soa.erp.api.sync.response;
+
+import com.gb.soa.omp.ccommon.api.response.MessagePack;
+
+public class ProductMcodeAndMpriceSyncResponse extends MessagePack {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7291799100201015034L;
+
+}
